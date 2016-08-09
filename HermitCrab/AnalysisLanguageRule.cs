df44dbeccb7024ca71eb317be96767c24caa5238
Damien Daspit <damien_daspit@sil.org>
2016-08-09 16:11:06 +0700

    Order of mrule unapplication is important
    * should not prune analyses that have the same rules unapplied but in a
    different order
    * unfortunately, this will make the new HC slower than the old HC
    * old HC was incorrect, but got away with it most of the time,
    because it performed a lexical lookup right after a rule was
    unapplied

--- HermitCrab/AnalysisLanguageRule.cs
+++ HermitCrab/AnalysisLanguageRule.cs
@@ -23,6 +23,7 @@ namespace SIL.HermitCrab
 		{
 			var inputSet = new HashSet<Word>(FreezableEqualityComparer<Word>.Default){input};
 			var tempSet = new HashSet<Word>(FreezableEqualityComparer<Word>.Default);
+			var results = new HashSet<Word>(FreezableEqualityComparer<Word>.Default);
 			for (int i = 0; i < _rules.Count && inputSet.Count > 0; i++)
 			{
 				if (!_morpher.RuleSelector(_strata[i]))
@@ -36,13 +37,15 @@ namespace SIL.HermitCrab
 					foreach (Word outData in _rules[i].Apply(inData))
 					{
 						outputSet.Add(outData);
-						yield return outData;
+						results.Add(outData);
 					}
 				}
 
 				tempSet = inputSet;
 				inputSet = outputSet;
 			}
+
+			return results;
 		}
 	}
 }
