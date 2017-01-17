1909d4618b8cbc5ed8566f5c1cdaab0593d83674
Damien Daspit <damien_daspit@sil.org>
2017-01-17 12:08:14 +0700

    Add TempDirectory class

--- tests/SIL.Machine.Translation.Thot.Tests/ThotBatchTrainerTests.cs
+++ tests/SIL.Machine.Translation.Thot.Tests/ThotBatchTrainerTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Threading;
 using NUnit.Framework;
+using SIL.IO;
 using SIL.Machine.Annotations;
 using SIL.Machine.Corpora;
 using SIL.Machine.Tokenization;
@@ -15,8 +14,7 @@ namespace SIL.Machine.Translation.Thot.Tests
 		[Test]
 		public void Train_NonEmptyCorpus_GeneratesModels()
 		{
-			string tempDir = CreateTempDirectory("ThotSmtEngineTests");
-			try
+			using (var tempDir = new TempDirectory("ThotSmtEngineTests"))
 			{
 				var spanFactory = new IntegerSpanFactory();
 				var tokenizer = new WhitespaceTokenizer(spanFactory);
@@ -44,83 +42,37 @@ namespace SIL.Machine.Translation.Thot.Tests
 							})
 					});
 
-				var trainer = new ThotBatchTrainer(Path.Combine(tempDir, "tm", "src_trg"), Path.Combine(tempDir, "lm", "trg.lm"), new ThotSmtParameters(),
+				var trainer = new ThotBatchTrainer(Path.Combine(tempDir.Path, "tm", "src_trg"), Path.Combine(tempDir.Path, "lm", "trg.lm"), new ThotSmtParameters(),
 					s => s, tokenizer, sourceCorpus, s => s, tokenizer, targetCorpus);
 				trainer.Train();
 
-				Assert.That(File.Exists(Path.Combine(tempDir, "lm", "trg.lm")), Is.True);
-				Assert.That(File.Exists(Path.Combine(tempDir, "tm", "src_trg_swm.hmm_alignd")), Is.True);
-				Assert.That(File.Exists(Path.Combine(tempDir, "tm", "src_trg_invswm.hmm_alignd")), Is.True);
-				Assert.That(File.Exists(Path.Combine(tempDir, "tm", "src_trg.ttable")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "lm", "trg.lm")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "tm", "src_trg_swm.hmm_alignd")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "tm", "src_trg_invswm.hmm_alignd")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "tm", "src_trg.ttable")), Is.True);
 				// TODO: test for more than just existence of files
-			}
-			finally
-			{
-				DeleteFolderThatMayBeInUse(tempDir);
 			}
 		}
 
 		[Test]
 		public void TrainModels_EmptyCorpus_GeneratesModels()
 		{
-			string tempDir = CreateTempDirectory("ThotSmtEngineTests");
-			try
+			using (var tempDir = new TempDirectory("ThotSmtEngineTests"))
 			{
 				var spanFactory = new IntegerSpanFactory();
 				var tokenizer = new WhitespaceTokenizer(spanFactory);
 				var sourceCorpus = new DictionaryTextCorpus(Enumerable.Empty<MemoryText>());
 				var targetCorpus = new DictionaryTextCorpus(Enumerable.Empty<MemoryText>());
 
-				var trainer = new ThotBatchTrainer(Path.Combine(tempDir, "tm", "src_trg"), Path.Combine(tempDir, "lm", "trg.lm"), new ThotSmtParameters(),
+				var trainer = new ThotBatchTrainer(Path.Combine(tempDir.Path, "tm", "src_trg"), Path.Combine(tempDir.Path, "lm", "trg.lm"), new ThotSmtParameters(),
 					s => s, tokenizer, sourceCorpus, s => s, tokenizer, targetCorpus);
 				trainer.Train();
 
-				Assert.That(File.Exists(Path.Combine(tempDir, "lm", "trg.lm")), Is.True);
-				Assert.That(File.Exists(Path.Combine(tempDir, "tm", "src_trg_swm.hmm_alignd")), Is.True);
-				Assert.That(File.Exists(Path.Combine(tempDir, "tm", "src_trg_invswm.hmm_alignd")), Is.True);
-				Assert.That(File.Exists(Path.Combine(tempDir, "tm", "src_trg.ttable")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "lm", "trg.lm")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "tm", "src_trg_swm.hmm_alignd")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "tm", "src_trg_invswm.hmm_alignd")), Is.True);
+				Assert.That(File.Exists(Path.Combine(tempDir.Path, "tm", "src_trg.ttable")), Is.True);
 				// TODO: test for more than just existence of files
-			}
-			finally
-			{
-				DeleteFolderThatMayBeInUse(tempDir);
-			}
-		}
-
-		private static string CreateTempDirectory(string name)
-		{
-			string path = Path.Combine(Path.GetTempPath(), name);
-			DeleteFolderThatMayBeInUse(path);
-			Directory.CreateDirectory(path);
-			return path;
-		}
-
-		private static void DeleteFolderThatMayBeInUse(string folder)
-		{
-			if (Directory.Exists(folder))
-			{
-				try
-				{
-					Directory.Delete(folder, true);
-				}
-				catch (Exception)
-				{
-					try
-					{
-						//maybe we can at least clear it out a bit
-						string[] files = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories);
-						foreach (string s in files)
-						{
-							File.Delete(s);
-						}
-						//sleep and try again (seems to work)
-						Thread.Sleep(1000);
-						Directory.Delete(folder, true);
-					}
-					catch (Exception)
-					{
-					}
-				}
 			}
 		}
 	}
