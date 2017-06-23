4f1f6503641d77f3e7d055f1e0b49f151d9be91d
Damien Daspit <damien_daspit@sil.org>
2017-06-23 15:40:35 +0700

    User lower camel case for JS members

--- src/SIL.Machine.JS/Properties/AssemblyInfo.cs
+++ src/SIL.Machine.JS/Properties/AssemblyInfo.cs
@@ -1,26 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using Bridge;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SIL.Machine.Translation.JS")]
+[assembly: AssemblyTitle("SIL.Machine.JS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SIL.Machine.Translation.JS")]
+[assembly: AssemblyProduct("SIL.Machine.JS")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ca453c16-d1da-4469-8bc3-62c67da44ece")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +25,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Convention(Target = ConventionTarget.Member, Notation = Notation.LowerCamelCase)]
--- tests/SIL.Machine.JS.Tests/Properties/AssemblyInfo.cs
+++ tests/SIL.Machine.JS.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using Bridge;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -14,14 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4e17df5d-e308-479a-a3e1-61e50b616f01")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -34,3 +25,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Convention(Target = ConventionTarget.Member, Notation = Notation.LowerCamelCase)]
