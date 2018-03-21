4e07d8ff9c20fc31405da28ee4fb7b3a298c4067
Damien Daspit <damien_daspit@sil.org>
2018-03-21 09:39:23 +0700

    Rsync contents of package folder

--- build/deploy-remote.sh
+++ build/deploy-remote.sh
@@ -5,7 +5,7 @@ source build.sh
 DEPLOY_PATH=${DEPLOY_PATH:-/opt/machine-web-api/}
 
 pushd .. > /dev/null
-rsync -vprogzlt --chown=root:www-data --delete-during --rsh="ssh -v -i $DEPLOY_CREDENTIALS" $BUILD_OUTPUT/package root@$DEPLOY_DESTINATION:$DEPLOY_PATH || exit 1
+rsync -vprogzlt --chown=root:www-data --delete-during --rsh="ssh -v -i $DEPLOY_CREDENTIALS" $BUILD_OUTPUT/package/ root@$DEPLOY_DESTINATION:$DEPLOY_PATH || exit 1
 popd > /dev/null
 
 ssh root@$DEPLOY_DESTINATION "systemctl restart machine-web-api"
