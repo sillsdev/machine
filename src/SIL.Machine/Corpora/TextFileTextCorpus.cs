70e68f8a1a27e31ef2a4a0e897123588aa3aa372
Damien Daspit <damien_daspit@sil.org>
2018-05-07 17:58:50 +0700

    Add support for different text file corpora in the same directory

--- src/SIL.Machine/Corpora/TextFileTextCorpus.cs
+++ src/SIL.Machine/Corpora/TextFileTextCorpus.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 using SIL.Machine.Tokenization;
+using SIL.PlatformUtilities;
 
 namespace SIL.Machine.Corpora
 {
@@ -23,14 +26,41 @@ namespace SIL.Machine.Corpora
 			{
 				string path = filePattern;
 				string searchPattern = "*";
-				if (!filePattern.EndsWith(Path.PathSeparator.ToString()))
+				if (!filePattern.EndsWith(Path.PathSeparator.ToString()) && !Directory.Exists(filePattern))
 				{
 					path = Path.GetDirectoryName(filePattern);
 					searchPattern = Path.GetFileName(filePattern);
 				}
 
+				if (path == "")
+					path = ".";
+
+				string convertedMask = "^" + Regex.Escape(Path.GetFileNameWithoutExtension(searchPattern))
+					.Replace("\\*", "(.*)").Replace("\\?", "(.)") + "$";
+				var maskRegex = new Regex(convertedMask,
+					Platform.IsWindows ? RegexOptions.IgnoreCase : RegexOptions.None);
+
 				foreach (string fileName in Directory.EnumerateFiles(path, searchPattern))
-					yield return new TextFileText(wordTokenizer, Path.GetFileNameWithoutExtension(fileName), fileName);
+				{
+					string id = Path.GetFileNameWithoutExtension(fileName);
+					Match match = maskRegex.Match(id);
+					if (match.Success)
+					{
+						var sb = new StringBuilder();
+						for (int i = 1; i < match.Groups.Count; i++)
+						{
+							if (!match.Groups[i].Success)
+								continue;
+
+							if (sb.Length > 0)
+								sb.Append("-");
+							sb.Append(match.Groups[i].Value);
+						}
+						if (sb.Length > 0)
+							id = sb.ToString();
+					}
+					yield return new TextFileText(wordTokenizer, id, fileName);
+				}
 			}
 		}
 	}
