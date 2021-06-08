2d9afdc9ca6aaa439625c335177d2b8a1cd2dadc
Damien Daspit <damien_daspit@sil.org>
2021-06-08 16:42:31 +0700

    Properly handle verse segments in ranges

--- src/SIL.Machine/Corpora/CorporaHelpers.cs
+++ src/SIL.Machine/Corpora/CorporaHelpers.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -80,27 +79,27 @@ namespace SIL.Machine.Corpora
 		internal static string MergeVerseRanges(string verse1, string verse2)
 		{
 			var sb = new StringBuilder();
-			int startVerseNum = -1;
-			int prevVerseNum = -1;
-			foreach (int verseNum in GetVerseNums(verse1).Union(GetVerseNums(verse2)).OrderBy(vn => vn))
+			(int, string) startVerseNum = (-1, null);
+			(int, string) prevVerseNum = (-1, null);
+			foreach ((int, string) verseNum in GetVerseNums(verse1).Union(GetVerseNums(verse2)).OrderBy(vn => vn.Item1))
 			{
-				if (prevVerseNum == -1)
+				if (prevVerseNum.Item1 == -1)
 				{
 					startVerseNum = verseNum;
 				}
-				else if (prevVerseNum != verseNum - 1)
+				else if (prevVerseNum.Item1 != verseNum.Item1 - 1)
 				{
-					AppendVerseRange(sb, startVerseNum, prevVerseNum);
+					AppendVerseRange(sb, startVerseNum.Item2, prevVerseNum.Item2);
 					startVerseNum = verseNum;
 				}
 				prevVerseNum = verseNum;
 			}
-			AppendVerseRange(sb, startVerseNum, prevVerseNum);
+			AppendVerseRange(sb, startVerseNum.Item2, prevVerseNum.Item2);
 
 			return sb.ToString();
 		}
 
-		private static void AppendVerseRange(StringBuilder sb, int startVerseNum, int endVerseNum)
+		private static void AppendVerseRange(StringBuilder sb, string startVerseNum, string endVerseNum)
 		{
 			if (sb.Length > 0)
 				sb.Append(VerseRef.verseSequenceIndicator);
@@ -112,23 +111,37 @@ namespace SIL.Machine.Corpora
 			}
 		}
 
-		private static IEnumerable<int> GetVerseNums(string verse)
+		private static IEnumerable<(int, string)> GetVerseNums(string verse)
 		{
 			string[] source = verse.Split(VerseRef.verseSequenceIndicators, StringSplitOptions.None);
 			foreach (string[] pieces in source.Select(part => part.Split(VerseRef.verseRangeSeparators,
 				StringSplitOptions.None)))
 			{
-				int startVerseNum = int.Parse(pieces[0], CultureInfo.InvariantCulture);
-				yield return startVerseNum;
+				int startVerseNum = GetVerseNum(pieces[0]);
+				yield return (startVerseNum, pieces[0]);
 				if (pieces.Length <= 1)
 					continue;
 
-				int endVerseNum = int.Parse(pieces[1], CultureInfo.InvariantCulture);
+				int endVerseNum = GetVerseNum(pieces[1]);
 				for (int verseNum = startVerseNum + 1; verseNum < endVerseNum; verseNum++)
-					yield return verseNum;
+					yield return (verseNum, verseNum.ToString());
 
-				yield return endVerseNum;
+				yield return (endVerseNum, pieces[1]);
 			}
+		}
+
+		private static int GetVerseNum(string verseStr)
+		{
+			int vNum = 0;
+			for (int i = 0; i < verseStr.Length; i++)
+			{
+				char ch = verseStr[i];
+				if (!char.IsDigit(ch))
+					break;
+
+				vNum = (int)(vNum * 10 + char.GetNumericValue(ch));
+			}
+			return vNum;
 		}
 	}
 }
--- tests/SIL.Machine.Tests/Corpora/UsfmFileTextTests.cs
+++ tests/SIL.Machine.Tests/Corpora/UsfmFileTextTests.cs
@@ -18,7 +18,7 @@ namespace SIL.Machine.Corpora
 
 			IText text = corpus.GetText("MAT");
 			TextSegment[] segments = text.Segments.ToArray();
-			Assert.That(segments.Length, Is.EqualTo(11));
+			Assert.That(segments.Length, Is.EqualTo(12));
 
 			Assert.That(segments[0].SegmentRef, Is.EqualTo(new VerseRef("MAT 1:1", corpus.Versification)));
 			Assert.That(segments[0].Segment[0], Is.EqualTo("Chapter one, verse one."));
@@ -34,22 +34,25 @@ namespace SIL.Machine.Corpora
 
 			Assert.That(segments[6].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:2", corpus.Versification)));
 			Assert.That(segments[6].Segment[0],
-				Is.EqualTo("Chapter two, verse two. Chapter two, verse three. Chapter two, verse four."));
+				Is.EqualTo("Chapter two, verse two. Chapter two, verse three."));
 			Assert.That(segments[6].IsInRange, Is.True);
 
 			Assert.That(segments[7].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:3", corpus.Versification)));
 			Assert.That(segments[7].Segment, Is.Empty);
 			Assert.That(segments[7].IsInRange, Is.True);
 
-			Assert.That(segments[8].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:4", corpus.Versification)));
+			Assert.That(segments[8].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:4a", corpus.Versification)));
 			Assert.That(segments[8].Segment, Is.Empty);
 			Assert.That(segments[8].IsInRange, Is.True);
 
-			Assert.That(segments[9].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:5", corpus.Versification)));
-			Assert.That(segments[9].Segment[0], Is.EqualTo("Chapter two, verse five."));
+			Assert.That(segments[9].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:4b", corpus.Versification)));
+			Assert.That(segments[9].Segment[0], Is.EqualTo("Chapter two, verse four."));
 
-			Assert.That(segments[10].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:6", corpus.Versification)));
-			Assert.That(segments[10].Segment[0], Is.EqualTo("Chapter two, verse six."));
+			Assert.That(segments[10].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:5", corpus.Versification)));
+			Assert.That(segments[10].Segment[0], Is.EqualTo("Chapter two, verse five."));
+
+			Assert.That(segments[11].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:6", corpus.Versification)));
+			Assert.That(segments[11].Segment[0], Is.EqualTo("Chapter two, verse six."));
 		}
 
 		[Test]
@@ -61,7 +64,7 @@ namespace SIL.Machine.Corpora
 
 			IText text = corpus.GetText("MAT");
 			TextSegment[] segments = text.Segments.ToArray();
-			Assert.That(segments.Length, Is.EqualTo(11));
+			Assert.That(segments.Length, Is.EqualTo(12));
 
 			Assert.That(segments[3].SegmentRef, Is.EqualTo(new VerseRef("MAT 1:4", corpus.Versification)));
 			Assert.That(segments[3].Segment[0], Is.EqualTo("Chapter one, verse four,"));
@@ -93,7 +96,7 @@ namespace SIL.Machine.Corpora
 
 			IText text = corpus.GetText("MAT");
 			TextSegment[] segments = text.Segments.ToArray();
-			Assert.That(segments.Length, Is.EqualTo(11));
+			Assert.That(segments.Length, Is.EqualTo(12));
 
 			Assert.That(segments[0].SegmentRef, Is.EqualTo(new VerseRef("MAT 1:1", corpus.Versification)));
 			Assert.That(segments[0].Segment[0], Is.EqualTo("Chapter one, verse one.\\f + \\fr 1:1: \\ft This is a footnote.\\f*"));
@@ -110,22 +113,25 @@ namespace SIL.Machine.Corpora
 
 			Assert.That(segments[6].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:2", corpus.Versification)));
 			Assert.That(segments[6].Segment[0],
-				Is.EqualTo("Chapter two, verse two. Chapter two, verse three. Chapter two, verse four."));
+				Is.EqualTo("Chapter two, verse two. Chapter two, verse three."));
 			Assert.That(segments[6].IsInRange, Is.True);
 
 			Assert.That(segments[7].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:3", corpus.Versification)));
 			Assert.That(segments[7].Segment, Is.Empty);
 			Assert.That(segments[7].IsInRange, Is.True);
 
-			Assert.That(segments[8].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:4", corpus.Versification)));
+			Assert.That(segments[8].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:4a", corpus.Versification)));
 			Assert.That(segments[8].Segment, Is.Empty);
 			Assert.That(segments[8].IsInRange, Is.True);
 
-			Assert.That(segments[9].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:5", corpus.Versification)));
-			Assert.That(segments[9].Segment[0], Is.EqualTo("Chapter two, verse five."));
+			Assert.That(segments[9].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:4b", corpus.Versification)));
+			Assert.That(segments[9].Segment[0], Is.EqualTo("Chapter two, verse four."));
 
-			Assert.That(segments[10].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:6", corpus.Versification)));
-			Assert.That(segments[10].Segment[0], Is.EqualTo("Chapter two, verse six."));
+			Assert.That(segments[10].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:5", corpus.Versification)));
+			Assert.That(segments[10].Segment[0], Is.EqualTo("Chapter two, verse five."));
+
+			Assert.That(segments[11].SegmentRef, Is.EqualTo(new VerseRef("MAT 2:6", corpus.Versification)));
+			Assert.That(segments[11].Segment[0], Is.EqualTo("Chapter two, verse six."));
 		}
 	}
 }
