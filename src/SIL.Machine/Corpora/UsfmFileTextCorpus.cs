bcd129193c82bbe57ed75c008717c56e510a7f0c
Damien Daspit <damien_daspit@sil.org>
2021-11-24 14:18:33 +0700

    Add ability to specify file pattern for UsfmFileTextCorpus

--- src/SIL.Machine/Corpora/UsfmFileTextCorpus.cs
+++ src/SIL.Machine/Corpora/UsfmFileTextCorpus.cs
@@ -8,12 +8,13 @@ namespace SIL.Machine.Corpora
 	public class UsfmFileTextCorpus : ScriptureTextCorpus
 	{
 		public UsfmFileTextCorpus(ITokenizer<string, int, string> wordTokenizer, string stylesheetFileName,
-			Encoding encoding, string projectPath, ScrVers versification = null, bool includeMarkers = false)
+			Encoding encoding, string projectPath, ScrVers versification = null, bool includeMarkers = false,
+			string filePattern = "*.SFM")
 			: base(wordTokenizer)
 		{
 			Versification = versification ?? ScrVers.English;
 			var stylesheet = new UsfmStylesheet(stylesheetFileName);
-			foreach (string sfmFileName in Directory.EnumerateFiles(projectPath, "*.SFM"))
+			foreach (string sfmFileName in Directory.EnumerateFiles(projectPath, filePattern))
 			{
 				AddText(new UsfmFileText(wordTokenizer, stylesheet, encoding, sfmFileName, Versification,
 					includeMarkers));
