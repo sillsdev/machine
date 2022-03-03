58911675c4dcea047779795e548f94d8fb029dcb
Damien Daspit <damien_daspit@sil.org>
2022-03-03 17:09:31 +0700

    Fix Timeout function

--- src/SIL.Machine/Threading/TaskExtensions.cs
+++ src/SIL.Machine/Threading/TaskExtensions.cs
@@ -16,9 +16,14 @@ namespace SIL.Machine.Threading
 		public static async Task Timeout(this Task task, TimeSpan timeout, CancellationToken cancellationToken)
 		{
 			if (timeout == System.Threading.Timeout.InfiniteTimeSpan)
+			{
 				await task;
-			Task t = await Task.WhenAny(task, Task.Delay(timeout, cancellationToken));
-			await t;
+			}
+			else
+			{
+				Task t = await Task.WhenAny(task, Task.Delay(timeout, cancellationToken));
+				await t;
+			}
 		}
 
 		private static async Task<TResult> Delay<TResult>(TimeSpan timeout, CancellationToken cancellationToken)
