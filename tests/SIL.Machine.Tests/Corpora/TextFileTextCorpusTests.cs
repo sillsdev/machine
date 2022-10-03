c390790488119d6f32e58bd24f875815b87a58c7
Damien Daspit <damien_daspit@sil.org>
2022-10-03 16:25:49 +0700

    Throw exception if text file doesn't exist

---
+++ tests/SIL.Machine.Tests/Corpora/TextFileTextCorpusTests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SIL.Machine.Corpora
+{
+    [TestFixture]
+    public class TextFileTextCorpusTests
+    {
+        [Test]
+        public void DoesNotExist()
+        {
+            Assert.Throws<FileNotFoundException>(() => new TextFileTextCorpus("does-not-exist.txt"));
+        }
+
+        [Test]
+        public void Folder()
+        {
+            var corpus = new TextFileTextCorpus(CorporaTestHelpers.TextTestProjectPath);
+
+            Assert.That(corpus.Texts.Select(t => t.Id), Is.EquivalentTo(new[] { "Test1", "Test2", "Test3" }));
+        }
+
+        [Test]
+        public void SingleFile()
+        {
+            var corpus = new TextFileTextCorpus(Path.Combine(CorporaTestHelpers.TextTestProjectPath, "Test1.txt"));
+
+            Assert.That(corpus.Texts.Select(t => t.Id), Is.EquivalentTo(new[] { "*all*" }));
+        }
+
+        [Test]
+        public void PatternStar()
+        {
+            var corpus = new TextFileTextCorpus(Path.Combine(CorporaTestHelpers.TextTestProjectPath, "*.txt"));
+
+            Assert.That(corpus.Texts.Select(t => t.Id), Is.EquivalentTo(new[] { "Test1", "Test2", "Test3" }));
+        }
+
+        [Test]
+        public void PatternQuestionMark()
+        {
+            var corpus = new TextFileTextCorpus(Path.Combine(CorporaTestHelpers.TextTestProjectPath, "Test?.txt"));
+
+            Assert.That(corpus.Texts.Select(t => t.Id), Is.EquivalentTo(new[] { "1", "2", "3" }));
+        }
+    }
+}
--- tests/SIL.Machine.Tests/Corpora/TextFileTextTests.cs
+++ tests/SIL.Machine.Tests/Corpora/TextFileTextTests.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using NUnit.Framework;
 
 namespace SIL.Machine.Corpora
@@ -10,47 +9,47 @@ namespace SIL.Machine.Corpora
         [Test]
         public void GetRows_NonEmptyTextMultiKeyRef()
         {
-            var corpus = new TextFileTextCorpus(Path.Combine(CorporaTestHelpers.TextTestProjectPath, "Test*.txt"));
-            IText text = corpus["1"];
+            var corpus = new TextFileTextCorpus(CorporaTestHelpers.TextTestProjectPath);
+            IText text = corpus["Test1"];
             TextRow[] segments = text.GetRows().ToArray();
             Assert.That(segments.Length, Is.EqualTo(4));
 
-            Assert.That(segments[0].Ref, Is.EqualTo(new MultiKeyRef("1", "s", 1, 1)));
+            Assert.That(segments[0].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 1, 1)));
             Assert.That(segments[0].Text, Is.EqualTo("Section one, line one."));
 
-            Assert.That(segments[1].Ref, Is.EqualTo(new MultiKeyRef("1", "s", 1, 2)));
+            Assert.That(segments[1].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 1, 2)));
             Assert.That(segments[1].Text, Is.EqualTo("Section one, line two."));
 
-            Assert.That(segments[2].Ref, Is.EqualTo(new MultiKeyRef("1", "s", 2, 1)));
+            Assert.That(segments[2].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 2, 1)));
             Assert.That(segments[2].Text, Is.EqualTo("Section two, line one."));
 
-            Assert.That(segments[3].Ref, Is.EqualTo(new MultiKeyRef("1", "s", 2, 2)));
+            Assert.That(segments[3].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 2, 2)));
             Assert.That(segments[3].Text, Is.EqualTo("Section two, line two."));
         }
 
         [Test]
         public void GetRows_NonEmptyTextTextFileRef()
         {
-            var corpus = new TextFileTextCorpus(Path.Combine(CorporaTestHelpers.TextTestProjectPath, "Test*.txt"));
-            IText text = corpus["3"];
+            var corpus = new TextFileTextCorpus(CorporaTestHelpers.TextTestProjectPath);
+            IText text = corpus["Test3"];
             TextRow[] segments = text.GetRows().ToArray();
             Assert.That(segments.Length, Is.EqualTo(3));
 
-            Assert.That(segments[0].Ref, Is.EqualTo(new MultiKeyRef("3", 1)));
+            Assert.That(segments[0].Ref, Is.EqualTo(new MultiKeyRef("Test3", 1)));
             Assert.That(segments[0].Text, Is.EqualTo("Line one."));
 
-            Assert.That(segments[1].Ref, Is.EqualTo(new MultiKeyRef("3", 2)));
+            Assert.That(segments[1].Ref, Is.EqualTo(new MultiKeyRef("Test3", 2)));
             Assert.That(segments[1].Text, Is.EqualTo("Line two."));
 
-            Assert.That(segments[2].Ref, Is.EqualTo(new MultiKeyRef("3", 3)));
+            Assert.That(segments[2].Ref, Is.EqualTo(new MultiKeyRef("Test3", 3)));
             Assert.That(segments[2].Text, Is.EqualTo("Line three."));
         }
 
         [Test]
         public void GetRows_EmptyText()
         {
-            var corpus = new TextFileTextCorpus(Path.Combine(CorporaTestHelpers.TextTestProjectPath, "Test*.txt"));
-            IText text = corpus["2"];
+            var corpus = new TextFileTextCorpus(CorporaTestHelpers.TextTestProjectPath);
+            IText text = corpus["Test2"];
             TextRow[] segments = text.GetRows().ToArray();
             Assert.That(segments, Is.Empty);
         }
