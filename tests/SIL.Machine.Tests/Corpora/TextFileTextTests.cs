64904e0a6e635e37bfbab7696bf1ab9786e4b067
Damien Daspit <damien_daspit@sil.org>
2022-12-20 15:10:16 +0700

    Use underscore as key separator for MultiKeyRef in TextFileText
    - do not automatically convert a number key from string to int

--- tests/SIL.Machine.Tests/Corpora/TextFileTextTests.cs
+++ tests/SIL.Machine.Tests/Corpora/TextFileTextTests.cs
@@ -14,26 +14,26 @@ namespace SIL.Machine.Corpora
             TextRow[] rows = text.GetRows().ToArray();
             Assert.That(rows.Length, Is.EqualTo(5));
 
-            Assert.That(rows[0].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 1, 1)));
+            Assert.That(rows[0].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", "1", "1")));
             Assert.That(rows[0].Text, Is.EqualTo("Section one, sentence one."));
             Assert.That(rows[0].Flags, Is.EqualTo(TextRowFlags.SentenceStart));
 
-            Assert.That(rows[1].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 1, 2)));
+            Assert.That(rows[1].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", "1", "2")));
             Assert.That(rows[1].Text, Is.EqualTo("Section one, sentence two. sentence three."));
             Assert.That(
                 rows[1].Flags,
                 Is.EqualTo(TextRowFlags.SentenceStart | TextRowFlags.InRange | TextRowFlags.RangeStart)
             );
 
-            Assert.That(rows[2].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 1, 3)));
+            Assert.That(rows[2].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", "1", "3")));
             Assert.That(rows[2].Text, Is.Empty);
             Assert.That(rows[2].Flags, Is.EqualTo(TextRowFlags.InRange));
 
-            Assert.That(rows[3].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 2, 1)));
+            Assert.That(rows[3].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", "2", "1")));
             Assert.That(rows[3].Text, Is.EqualTo("Section two, sentence one."));
             Assert.That(rows[3].Flags, Is.EqualTo(TextRowFlags.SentenceStart));
 
-            Assert.That(rows[4].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", 2, 2)));
+            Assert.That(rows[4].Ref, Is.EqualTo(new MultiKeyRef("Test1", "s", "2", "2")));
             Assert.That(rows[4].Text, Is.EqualTo("Section two, sentence two."));
             Assert.That(rows[4].Flags, Is.EqualTo(TextRowFlags.None));
         }
