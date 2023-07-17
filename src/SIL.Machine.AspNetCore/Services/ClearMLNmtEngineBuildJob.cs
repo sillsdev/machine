b418bedff140cd8ebe3440893618986da7702ccc
Enkidu93 <lowryec17@gcc.edu>
2023-07-17 09:18:09 -0400

    Minor error-handling change (most was done on serval side)
    An attempt at an S3 bucket health check but having some issues with it logging
    
    --ECL

--- src/SIL.Machine.AspNetCore/Services/ClearMLNmtEngineBuildJob.cs
+++ src/SIL.Machine.AspNetCore/Services/ClearMLNmtEngineBuildJob.cs
@@ -218,21 +218,30 @@ public class ClearMLNmtEngineBuildJob
         }
         catch (Exception e)
         {
-            await _sharedFileService.DeleteAsync($"builds/{buildId}/", CancellationToken.None);
+            try
+            {
+                await _sharedFileService.DeleteAsync($"builds/{buildId}/", CancellationToken.None);
+            }
+            catch (HttpRequestException)
+            {
+                _logger.LogError("Unable to access S3 bucket. Likely, the AWS credentials are invalid.");
+            }
+            finally
+            {
+                await _engines.UpdateAsync(
+                    e => e.EngineId == engineId && e.BuildId == buildId,
+                    u =>
+                        u.Set(e => e.BuildState, BuildState.None)
+                            .Set(e => e.IsCanceled, false)
+                            .Unset(e => e.JobId)
+                            .Unset(e => e.BuildId),
+                    cancellationToken: CancellationToken.None
+                );
 
-            await _engines.UpdateAsync(
-                e => e.EngineId == engineId && e.BuildId == buildId,
-                u =>
-                    u.Set(e => e.BuildState, BuildState.None)
-                        .Set(e => e.IsCanceled, false)
-                        .Unset(e => e.JobId)
-                        .Unset(e => e.BuildId),
-                cancellationToken: CancellationToken.None
-            );
-
-            await _platformService.BuildFaultedAsync(buildId, e.Message, CancellationToken.None);
-            _logger.LogError(0, e, "Build faulted ({0})", buildId);
-            throw;
+                await _platformService.BuildFaultedAsync(buildId, e.Message, CancellationToken.None);
+                _logger.LogError(0, e, "Build faulted ({0})", buildId);
+                throw e;
+            }
         }
     }
 
