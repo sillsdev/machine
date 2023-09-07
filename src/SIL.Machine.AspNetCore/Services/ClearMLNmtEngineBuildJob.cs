a736a896f4aa010ac3a7be61ef19210431c2c303
John Lambert <john_lambert@sil.org>
2023-09-07 09:21:09 -0400

    #75 - don't fail nmt job on delete failing

--- src/SIL.Machine.AspNetCore/Services/ClearMLNmtEngineBuildJob.cs
+++ src/SIL.Machine.AspNetCore/Services/ClearMLNmtEngineBuildJob.cs
@@ -140,7 +140,17 @@ public class ClearMLNmtEngineBuildJob
                 CancellationToken.None
             );
 
-            await _sharedFileService.DeleteAsync($"builds/{buildId}/", CancellationToken.None);
+            try
+            {
+                //Don't fail the whole job if we can't delete the files.
+                await _sharedFileService.DeleteAsync($"builds/{buildId}/", CancellationToken.None);
+            }
+            catch (AmazonS3Exception e)
+            {
+                _logger.LogError(
+                    $"Could not delete build ({buildId}) because of exception {e.GetType().Name}:{e.Message}.  Check S3 bucket key permissions.  Finishing up build anyway."
+                );
+            }
 
             await _engines.UpdateAsync(
                 e => e.EngineId == engineId && e.BuildId == buildId,
