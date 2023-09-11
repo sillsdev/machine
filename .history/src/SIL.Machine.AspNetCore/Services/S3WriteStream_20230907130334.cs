bb364f31828b26f5487825a4346f6417a7cbe1c7
Enkidu93 <lowryec17@gcc.edu>
2023-09-11 13:02:29 -0400

    Empty commit to trigger workflow --ECL

---
+++ .history/src/SIL.Machine.AspNetCore/Services/S3WriteStream_20230907130334.cs
@@ -0,0 +1,78 @@
+namespace SIL.Machine.AspNetCore.Services;
+
+public class S3WriteStream : Stream
+{
+    private readonly AmazonS3Client _client;
+    private readonly string _key;
+    private readonly string _uploadId;
+    private readonly string _bucketName;
+    private readonly List<UploadPartResponse> uploadResponses = new();
+    private long _length;
+
+    public S3WriteStream(AmazonS3Client client, string key, string bucketName, string uploadId)
+    {
+        _client = client;
+        _key = key;
+        _bucketName = bucketName;
+        _uploadId = uploadId;
+    }
+
+    public override bool CanRead => false;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => true;
+
+    public override long Length => _length;
+
+    public override long Position
+    {
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
+
+    public override void Flush() { }
+
+    public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+    public override void SetLength(long value) => throw new NotSupportedException();
+
+    public override Task FlushAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        using Stream inputStream = new MemoryStream(buffer, offset, count);
+        using var transferUtility = new TransferUtility(_client);
+        var uploadRequest = new TransferUtilityUploadRequest
+        {
+            BucketName = _bucketName,
+            InputStream = inputStream,
+            Key = _key,
+            PartSize = count
+        };
+        transferUtility.Upload(uploadRequest);
+    }
+
+    public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
+        using Stream inputStream = new MemoryStream(buffer, offset, count);
+        using var transferUtility = new TransferUtility(_client);
+        var uploadRequest = new TransferUtilityUploadRequest
+        {
+            BucketName = _bucketName,
+            InputStream = inputStream,
+            Key = _key,
+            PartSize = count
+        };
+        await transferUtility.UploadAsync(uploadRequest);
+    }
+
+    public override ValueTask DisposeAsync()
+    {
+        Dispose(disposing: false);
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
