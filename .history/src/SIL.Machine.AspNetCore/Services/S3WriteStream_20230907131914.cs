c9384c86e4886efa63fa416e76571f0c88b7f0e6
Enkidu93 <lowryec17@gcc.edu>
2023-09-11 13:02:29 -0400

    Empty commit to trigger workflow --ECL

---
+++ .history/src/SIL.Machine.AspNetCore/Services/S3WriteStream_20230907131914.cs
@@ -0,0 +1,115 @@
+namespace SIL.Machine.AspNetCore.Services;
+
+public class S3WriteStream : Stream
+{
+    private readonly AmazonS3Client _client;
+    private readonly string _key;
+    private readonly string _uploadId;
+    private readonly string _bucketName;
+    private readonly List<UploadPartResponse> uploadResponses = new();
+    private readonly ILogger<S3WriteStream> _logger;
+
+    public S3WriteStream(AmazonS3Client client, string key, string bucketName, string uploadId)
+    {
+        _client = client;
+        _key = key;
+        _bucketName = bucketName;
+        _uploadId = uploadId;
+        _logger = (new LoggerFactory()).CreateLogger<S3WriteStream>();
+    }
+
+    public override bool CanRead => false;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => true;
+
+    public override long Length => 0;
+
+    public override long Position
+    {
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
+
+    public override void Flush() { }
+
+    public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+    public override void SetLength(long value) => throw new NotSupportedException();
+
+    public override Task FlushAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+    public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
+        try
+        {
+            using MemoryStream ms = new(buffer, offset, count);
+            int partNumber = uploadResponses.Count + 1;
+            UploadPartRequest request =
+                new()
+                {
+                    BucketName = _bucketName,
+                    Key = _key,
+                    UploadId = _uploadId,
+                    PartNumber = partNumber,
+                    InputStream = ms
+                };
+            request.StreamTransferProgress += new EventHandler<StreamTransferProgressArgs>((_, e) => { });
+            UploadPartResponse response = await _client.UploadPartAsync(request);
+            if (response.HttpStatusCode != HttpStatusCode.OK)
+                throw new HttpRequestException(
+                    $"Uploading part {partNumber} of upload {_uploadId} to {_bucketName}/{_key} but received response code {response.HttpStatusCode}"
+                );
+            uploadResponses.Add(response);
+        }
+        catch (Exception)
+        {
+            await Abort();
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+    }
+
+    public async override ValueTask DisposeAsync()
+    {
+        try
+        {
+            CompleteMultipartUploadRequest request =
+                new()
+                {
+                    BucketName = _bucketName,
+                    Key = _key,
+                    UploadId = _uploadId
+                };
+            request.AddPartETags(uploadResponses);
+            await _client.CompleteMultipartUploadAsync(request);
+            Dispose(disposing: false);
+            GC.SuppressFinalize(this);
+        }
+        catch (Exception)
+        {
+            await Abort();
+        }
+    }
+
+    private async Task Abort()
+    {
+        // Logging?
+        AbortMultipartUploadRequest abortMPURequest =
+            new()
+            {
+                BucketName = _bucketName,
+                Key = _key,
+                UploadId = _uploadId
+            };
+        await _client.AbortMultipartUploadAsync(abortMPURequest);
+    }
+}
---
+++ .history/src/SIL.Machine.AspNetCore/Services/SharedFileService_20230908125406.cs
@@ -0,0 +1,96 @@
+﻿namespace SIL.Machine.AspNetCore.Services;
+
+public class SharedFileService : ISharedFileService
+{
+    private readonly Uri? _baseUri;
+    private readonly FileStorage _fileStorage;
+    private readonly bool _supportFolderDelete = true;
+    private readonly ILoggerFactory _loggerFactory;
+
+    public SharedFileService(ILoggerFactory loggerFactory, IOptions<SharedFileOptions>? options = null)
+    {
+        _loggerFactory = loggerFactory;
+
+        if (options?.Value.Uri is null)
+        {
+            _fileStorage = new InMemoryStorage();
+        }
+        else
+        {
+            string baseUri = options.Value.Uri;
+            if (!baseUri.EndsWith("/"))
+                baseUri += "/";
+            _baseUri = new Uri(baseUri);
+            switch (_baseUri.Scheme)
+            {
+                case "file":
+                    _fileStorage = new LocalStorage(_baseUri.LocalPath);
+                    Directory.CreateDirectory(_baseUri.LocalPath);
+                    break;
+                case "s3":
+                    _fileStorage = new S3FileStorage(
+                        _baseUri.Host,
+                        _baseUri.AbsolutePath,
+                        options.Value.S3AccessKeyId,
+                        options.Value.S3SecretAccessKey,
+                        options.Value.S3Region,
+                        _loggerFactory
+                    );
+                    _supportFolderDelete = false;
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unsupported URI scheme: {_baseUri.Scheme}");
+            }
+        }
+    }
+
+    public Uri GetBaseUri()
+    {
+        return GetResolvedUri("");
+    }
+
+    public Uri GetResolvedUri(string path)
+    {
+        if (_baseUri is null)
+            return new Uri($"memory://{path}");
+        return new Uri(_baseUri, path);
+    }
+
+    public Task<Stream> OpenReadAsync(string path, CancellationToken cancellationToken = default)
+    {
+        return _fileStorage.OpenRead(path, cancellationToken);
+    }
+
+    public Task<Stream> OpenWriteAsync(string path, CancellationToken cancellationToken = default)
+    {
+        return _fileStorage.OpenWrite(path, cancellationToken);
+    }
+
+    public async Task DeleteAsync(string path, CancellationToken cancellationToken = default)
+    {
+        if (!_supportFolderDelete && path.EndsWith("/"))
+        {
+            IReadOnlyCollection<string> files = await _fileStorage.Ls(path, recurse: true, cancellationToken);
+            foreach (string file in files)
+                await _fileStorage.Rm(file, cancellationToken: cancellationToken);
+        }
+        else
+        {
+            await _fileStorage.Rm(path, recurse: true, cancellationToken: cancellationToken);
+        }
+    }
+
+    public Task<bool> ExistsAsync(string path, CancellationToken cancellationToken = default)
+    {
+        return _fileStorage.Exists(path, cancellationToken);
+    }
+
+    public Task<IReadOnlyCollection<string>> Ls(
+        string path,
+        bool recurse = false,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return _fileStorage.Ls(path, recurse, cancellationToken);
+    }
+}
