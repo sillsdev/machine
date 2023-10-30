acb60b1a5f86e114162f57e879f93701a2f33c83
John Lambert <john_lambert@sil.org>
2023-10-30 14:40:04 -0400

    Update some logic, flow and documentation of parsing out language codes

--- src/SIL.Machine.AspNetCore/Services/LanguageTagService.cs
+++ src/SIL.Machine.AspNetCore/Services/LanguageTagService.cs
@@ -7,15 +7,29 @@ public class LanguageTagService : ILanguageTagService
 
     private readonly Dictionary<string, string> _defaultScripts;
 
+    private readonly Dictionary<string, string> _threeToTwo;
+
+    private readonly Regex _langTagPattern;
+
     public LanguageTagService()
     {
         // initialise SLDR language tags to retrieve latest langtags.json file
         Sldr.InitializeLanguageTags();
+        _defaultScripts = InitializeDefaultScripts();
+        _threeToTwo = InitializeThreeToTwo();
+        _langTagPattern = new Regex(
+            "(?'language'[a-zA-Z]{2,8})([_-](?'script'[a-zA-Z]{4}))?",
+            RegexOptions.ExplicitCapture
+        );
+    }
+
+    private Dictionary<string, string> InitializeDefaultScripts()
+    {
         var cachedAllTagsPath = Path.Combine(Sldr.SldrCachePath, "langtags.json");
         using var stream = new FileStream(cachedAllTagsPath, FileMode.Open);
 
         var json = JsonNode.Parse(stream);
-        _defaultScripts = new Dictionary<string, string>();
+        var tempDefaultScripts = new Dictionary<string, string>();
         foreach (JsonNode? entry in json!.AsArray())
         {
             if (entry is null)
@@ -34,35 +48,70 @@ public class LanguageTagService : ILanguageTagService
                         t is not null && IetfLanguageTag.TryGetParts(t, out _, out string? s, out _, out _) && s is null
                     )
                     {
-                        _defaultScripts[t] = script;
+                        tempDefaultScripts[t] = script;
                     }
                 }
             }
 
             var tag = (string?)entry["tag"];
             if (tag is not null)
-                _defaultScripts[tag] = script;
+                tempDefaultScripts[tag] = script;
         }
+        return tempDefaultScripts;
+    }
+
+    private Dictionary<string, string> InitializeThreeToTwo()
+    {
+        var temp3to2 = new Dictionary<string, string>();
+        foreach (LanguageSubtag l in StandardSubtags.RegisteredLanguages)
+        {
+            if (l.Iso3Code.Length > 0)
+            {
+                temp3to2[l.Iso3Code] = l.Code;
+            }
+        }
+        return temp3to2;
     }
 
     public string ConvertToFlores200Code(string languageTag)
     {
-        if (!IetfLanguageTag.TryGetParts(languageTag, out string? language, out string? script, out _, out _))
+        // Try to find a pattern of {language code}_{script}
+        Match langTagMatch = _langTagPattern.Match(languageTag);
+        if (!langTagMatch.Success)
             return languageTag;
+        string parsed_language = langTagMatch.Groups["language"].Value;
+        string language_subtag = parsed_language;
+        string iso639_3_code = parsed_language;
 
-        if (!StandardSubtags.RegisteredLanguages.TryGet(language, out LanguageSubtag languageSubtag))
-            return languageTag;
+        // Best attempt to convert language to a registered ISO 639-3 code
+        // Uses https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry for mapping
 
-        // Normalize to a standard language subtag
-        if (StandardLanguages.TryGetValue(language, out string? standardLanguageCode))
-            languageSubtag = StandardSubtags.RegisteredLanguages[standardLanguageCode];
+        // If they gave us the ISO code, revert it to the 2 character code
+        if (_threeToTwo.ContainsKey(language_subtag))
+            language_subtag = _threeToTwo[language_subtag];
+
+        // There are a few extra conversions not in SIL Writing Systems that we need to handle
+        if (StandardLanguages.ContainsKey(language_subtag))
+            language_subtag = StandardLanguages[language_subtag];
+
+        if (StandardSubtags.RegisteredLanguages.Contains(language_subtag))
+            iso639_3_code = StandardSubtags.RegisteredLanguages[language_subtag].Iso3Code;
+
+        // Use default script unless there is one parsed out of the language tag
+        Group scriptGroup = langTagMatch.Groups["script"];
+        string? script = null;
+        if (_defaultScripts.ContainsKey(language_subtag))
+            script = _defaultScripts[language_subtag];
+
+        if (_defaultScripts.ContainsKey(languageTag))
+            script = _defaultScripts[languageTag];
+
+        if (scriptGroup.Success)
+            script = scriptGroup.Value;
 
         if (script is not null)
-            return $"{languageSubtag.Iso3Code}_{script}";
-
-        if (_defaultScripts.TryGetValue(languageTag, out script))
-            return $"{languageSubtag.Iso3Code}_{script}";
-
-        return languageTag;
+            return $"{iso639_3_code}_{script}";
+        else
+            return languageTag;
     }
 }
--- src/SIL.Machine.AspNetCore/Usings.cs
+++ src/SIL.Machine.AspNetCore/Usings.cs
@@ -10,6 +10,7 @@ global using System.Security.Cryptography;
 global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Nodes;
+global using System.Text.RegularExpressions;
 global using Amazon;
 global using Amazon.Runtime;
 global using Amazon.S3;
