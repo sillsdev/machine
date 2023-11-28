aade573dce7bd68cc5268cdc59001d0af42c8d9c
Enkidu93 <lowryec17@gcc.edu>
2023-11-28 15:25:23 -0500

    Working native key terms functionality

--- src/SIL.Machine/Corpora/ParatextKeyTermsCorpus.cs
+++ src/SIL.Machine/Corpora/ParatextKeyTermsCorpus.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using System.Linq;
@@ -9,42 +10,72 @@ namespace SIL.Machine.Corpora
 {
     public class ParatextKeyTermsCorpus : DictionaryTextCorpus
     {
+        public string BiblicalTermsType { get; set; }
+
         public ParatextKeyTermsCorpus(string projectDir)
         {
             List<TextRow> rows = new List<TextRow>();
-            string keyTermsFileName = Path.Combine(projectDir, "ProjectBiblicalTerms.xml");
-            if (!File.Exists(keyTermsFileName))
+            using (var archive = ZipFile.OpenRead(projectDir))
             {
-                throw new ArgumentException(
-                    "The project directory does not contain a key terms file.",
-                    nameof(projectDir)
-                );
-            }
-            XDocument keyTermsDoc = XDocument.Load(keyTermsFileName);
-            foreach (XElement element in keyTermsDoc.Elements("Term"))
-            {
-                string id = element.Attributes("Id").First().Name.LocalName;
-                id = id.Replace("\n", "&#xA");
-                string gloss = element.Elements("Gloss").First().Value;
-                Regex rx = new Regex(@"\[(.+?)\]", RegexOptions.Compiled);
-                Match match = rx.Match(gloss);
-                if (match.Success)
-                    gloss = match.Groups[0].Value;
-                gloss = gloss.Replace("?", "");
-                gloss = gloss.Trim();
-                gloss = StripParens(gloss);
-                gloss = StripParens(gloss, left: '[', right: ']');
-                Regex rx2 = new Regex(@"\s+\d+(\.\d+)*$", RegexOptions.Compiled);
-                foreach (Match m in rx2.Matches(gloss))
+                ZipArchiveEntry keyTermsFileEntry = archive.GetEntry("ProjectBiblicalTerms.xml");
+                if (keyTermsFileEntry is null)
                 {
-                    gloss.Replace(m.Value, "");
+                    throw new ArgumentException(
+                        $"The project directory does not contain a key terms file",
+                        nameof(projectDir)
+                    );
                 }
-                IEnumerable<string> glosses = gloss.Split(';', ',', '/');
-                glosses = glosses.Select(g => g.Trim()).Where(s => s != "").Distinct();
-                rows.Add(new TextRow(projectDir + "_KeyTerms", id) { Segment = (IReadOnlyList<string>)glosses });
+                ZipArchiveEntry settingsEntry = archive.GetEntry("Settings.xml");
+                if (settingsEntry == null)
+                    settingsEntry = archive.Entries.FirstOrDefault(e => e.FullName.EndsWith(".ssf"));
+                if (settingsEntry == null)
+                {
+                    throw new ArgumentException(
+                        "The project backup does not contain a settings file.",
+                        nameof(projectDir)
+                    );
+                }
+                XDocument settingsDoc;
+                using (Stream stream = settingsEntry.Open())
+                {
+                    settingsDoc = XDocument.Load(stream);
+                }
+                BiblicalTermsType = settingsDoc.Root.Element("BiblicalTermsListSetting").Value;
+                XDocument keyTermsDoc;
+                using (var keyTermsFile = keyTermsFileEntry.Open())
+                {
+                    keyTermsDoc = XDocument.Load(keyTermsFile);
+                }
+                IEnumerable<XElement> keyTermsElements = keyTermsDoc
+                    .Descendants()
+                    .Where(n => n.Name.LocalName == "Term");
+                foreach (XElement element in keyTermsElements)
+                {
+                    if ((element.Element("Category")?.Value ?? "") != "PN")
+                        continue;
+                    string id = element.Attribute("Id").Value;
+                    id = id.Replace("\n", "&#xA");
+                    string gloss = element.Element("Gloss").Value;
+                    Regex rx = new Regex(@"\[(.+?)\]", RegexOptions.Compiled);
+                    Match match = rx.Match(gloss);
+                    if (match.Success)
+                        gloss = match.Groups[0].Value;
+                    gloss = gloss.Replace("?", "");
+                    gloss = gloss.Trim();
+                    gloss = StripParens(gloss);
+                    gloss = StripParens(gloss, left: '[', right: ']');
+                    Regex rx2 = new Regex(@"\s+\d+(\.\d+)*$", RegexOptions.Compiled);
+                    foreach (Match m in rx2.Matches(gloss))
+                    {
+                        gloss.Replace(m.Value, "");
+                    }
+                    IEnumerable<string> glosses = gloss.Split(';', ',', '/');
+                    glosses = glosses.Select(g => g.Trim()).Where(s => s != "").Distinct().ToList();
+                    rows.Add(new TextRow("KeyTerms", id) { Segment = (IReadOnlyList<string>)glosses });
+                }
+                IText text = new MemoryText("KeyTerms", rows);
+                AddText(text);
             }
-            IText text = new MemoryText(projectDir + "_KeyTerms", rows); //TODO id?
-            AddText(text);
         }
 
         private static string StripParens(string termString, char left = '(', char right = ')')
