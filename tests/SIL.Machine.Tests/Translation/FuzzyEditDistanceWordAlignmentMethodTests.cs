f95b68c411898f30cb0934822185b6bd0608de48
Damien Daspit <damien_daspit@sil.org>
2023-11-30 16:23:42 -0500

    Fix NaN alignment score in FuzzyEditDistanceWordAlignmentMethod
    - fixes #145

---
+++ tests/SIL.Machine.Tests/Translation/FuzzyEditDistanceWordAlignmentMethodTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace SIL.Machine.Translation
+{
+    [TestFixture]
+    public class FuzzyEditDistanceWordAlignmentMethodTests
+    {
+        [Test]
+        public void Align_LastSrcFirstTrg()
+        {
+            var method = new FuzzyEditDistanceWordAlignmentMethod
+            {
+                ScoreSelector = (srcSegment, srcIndex, trgSegment, trgIndex) =>
+                {
+                    if (srcIndex == -1 || trgIndex == -1)
+                        return 0.1;
+                    return srcSegment[srcIndex] == trgSegment[trgIndex] ? 0.9 : 0.1;
+                }
+            };
+
+            WordAlignmentMatrix matrix = method.Align("A B".Split(), "B C".Split());
+            Assert.That(matrix.ToString(), Is.EqualTo("1-0"));
+        }
+    }
+}
