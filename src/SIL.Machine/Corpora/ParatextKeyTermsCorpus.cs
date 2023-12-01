ba0155a45f97df766bd01d928805ab41bfc14534
Enkidu93 <lowryec17@gcc.edu>
2023-12-01 09:50:07 -0500

    Working key terms solution including: *Testing *Moving logic (as possible) out of the build job *Added logic to SMT *Stuck with buildOptions for configuring (after further conversation)
    Issues - Not as much abstraction as I'd like between the CorpusService, *BuildJobs, etc.

--- src/SIL.Machine/Corpora/ParatextKeyTermsCorpus.cs
+++ src/SIL.Machine/Corpora/ParatextKeyTermsCorpus.cs
@@ -17,11 +17,11 @@ namespace SIL.Machine.Corpora
             List<TextRow> rows = new List<TextRow>();
             using (var archive = ZipFile.OpenRead(projectDir))
             {
-                ZipArchiveEntry keyTermsFileEntry = archive.GetEntry("ProjectBiblicalTerms.xml");
-                if (keyTermsFileEntry is null)
+                ZipArchiveEntry termsFileEntry = archive.GetEntry("TermRenderings.xml");
+                if (termsFileEntry is null)
                 {
                     throw new ArgumentException(
-                        $"The project directory does not contain a key terms file",
+                        $"The project directory does not contain a term renderings file",
                         nameof(projectDir)
                     );
                 }
@@ -41,44 +41,58 @@ namespace SIL.Machine.Corpora
                     settingsDoc = XDocument.Load(stream);
                 }
                 BiblicalTermsType = settingsDoc.Root.Element("BiblicalTermsListSetting").Value;
-                XDocument keyTermsDoc;
-                using (var keyTermsFile = keyTermsFileEntry.Open())
+                XDocument termsDoc;
+                using (var keyTermsFile = termsFileEntry.Open())
                 {
-                    keyTermsDoc = XDocument.Load(keyTermsFile);
+                    termsDoc = XDocument.Load(keyTermsFile);
                 }
-                IEnumerable<XElement> keyTermsElements = keyTermsDoc
+                IEnumerable<XElement> termsElements = termsDoc
                     .Descendants()
-                    .Where(n => n.Name.LocalName == "Term");
-                foreach (XElement element in keyTermsElements)
+                    .Where(n => n.Name.LocalName == "TermRendering");
+                foreach (XElement element in termsElements)
                 {
-                    if ((element.Element("Category")?.Value ?? "") != "PN")
-                        continue;
                     string id = element.Attribute("Id").Value;
                     id = id.Replace("\n", "&#xA");
-                    string gloss = element.Element("Gloss").Value;
-                    Regex rx = new Regex(@"\[(.+?)\]", RegexOptions.Compiled);
-                    Match match = rx.Match(gloss);
-                    if (match.Success)
-                        gloss = match.Groups[0].Value;
-                    gloss = gloss.Replace("?", "");
-                    gloss = gloss.Trim();
-                    gloss = StripParens(gloss);
-                    gloss = StripParens(gloss, left: '[', right: ']');
-                    Regex rx2 = new Regex(@"\s+\d+(\.\d+)*$", RegexOptions.Compiled);
-                    foreach (Match m in rx2.Matches(gloss))
-                    {
-                        gloss.Replace(m.Value, "");
-                    }
-                    IEnumerable<string> glosses = gloss.Split(';', ',', '/');
-                    glosses = glosses.Select(g => g.Trim()).Where(s => s != "").Distinct().ToList();
-                    rows.Add(new TextRow("KeyTerms", id) { Segment = (IReadOnlyList<string>)glosses });
+                    string gloss = element.Element("Renderings").Value;
+                    IReadOnlyList<string> glosses = GetGlosses(gloss);
+                    rows.Add(new TextRow("KeyTerms", id) { Segment = glosses });
                 }
                 IText text = new MemoryText("KeyTerms", rows);
                 AddText(text);
             }
         }
 
-        private static string StripParens(string termString, char left = '(', char right = ')')
+        public static IReadOnlyList<string> GetGlosses(string gloss)
+        {
+            //If entire term rednering is surrounded in square brackets, remove them
+            Regex rx = new Regex(@"^\[(.+?)\]$", RegexOptions.Compiled);
+            Match match = rx.Match(gloss);
+            if (match.Success)
+                gloss = match.Groups[0].Value;
+            gloss = gloss.Replace("?", "");
+            gloss = gloss.Replace("*", "");
+            gloss = gloss.Replace("/", " ");
+            gloss = gloss.Trim();
+            gloss = StripParens(gloss);
+            gloss = StripParens(gloss, left: '[', right: ']');
+            Regex rx2 = new Regex(@"\s+\d+(\.\d+)*$", RegexOptions.Compiled);
+            foreach (Match m in rx2.Matches(gloss))
+            {
+                gloss.Replace(m.Value, "");
+            }
+            IEnumerable<string> glosses = Regex.Split(gloss, @"\|\|");
+            glosses = glosses.Select(g => g.Trim()).Where(s => s != "").Distinct().ToList();
+            return (IReadOnlyList<string>)glosses;
+        }
+
+        /// <summary>
+        /// Strips all content between left and right parentheses "left" and "right" and returns resultant string
+        /// </summary>
+        /// <param name="termString">The string to be modified</param>
+        /// <param name="left">The desired left parenthesis char e.g. [,{,(, etc.</param>
+        /// <param name="right">The desired right parenthesis char e.g. ],},) etc.</param>
+        /// <returns>String stripped of content between parentheses</returns>
+        public static string StripParens(string termString, char left = '(', char right = ')')
         {
             int parens = 0;
             int end = -1;
