05c2ee7b1f23327d3aa9cb159f0acd50e236d450
Enkidu93 <lowryec17@gcc.edu>
2023-12-11 11:07:04 -0500

    Revert to using CustomEnumConverter

--- src/SIL.Machine.AspNetCore/Usings.cs
+++ src/SIL.Machine.AspNetCore/Usings.cs
@@ -8,6 +8,7 @@ global using System.Reflection;
 global using System.Runtime.CompilerServices;
 global using System.Security.Cryptography;
 global using System.Text;
+global using System.Text.Encodings.Web;
 global using System.Text.Json;
 global using System.Text.Json.Nodes;
 global using System.Text.Json.Serialization;
