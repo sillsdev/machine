9acec247a94fb2f9b778969cab83467ae1a9f0d1
Damien Daspit <damien_daspit@sil.org>
2024-01-15 17:52:53 -0500

    Add UsfmVerseTextUpdater class

--- src/SIL.Machine/Corpora/ParatextBackupTextCorpus.cs
+++ src/SIL.Machine/Corpora/ParatextBackupTextCorpus.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.IO;
-using System.IO.Compression;
+﻿using System.IO.Compression;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using SIL.IO;
-using SIL.Scripture;
 
 namespace SIL.Machine.Corpora
 {
@@ -14,104 +8,23 @@ namespace SIL.Machine.Corpora
     {
         public ParatextBackupTextCorpus(string fileName, bool includeMarkers = false)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             using (ZipArchive archive = ZipFile.OpenRead(fileName))
             {
-                ZipArchiveEntry settingsEntry = archive.GetEntry("Settings.xml");
-                if (settingsEntry == null)
-                    settingsEntry = archive.Entries.FirstOrDefault(e => e.FullName.EndsWith(".ssf"));
-                if (settingsEntry == null)
-                {
-                    throw new ArgumentException(
-                        "The project backup does not contain a settings file.",
-                        nameof(fileName)
-                    );
-                }
-                XDocument settingsDoc;
-                using (Stream stream = settingsEntry.Open())
-                {
-                    settingsDoc = XDocument.Load(stream);
-                }
-                var encodingStr = (string)settingsDoc.Root.Element("Encoding") ?? "65001";
-                if (!int.TryParse(encodingStr, out int codePage))
-                {
-                    throw new NotImplementedException(
-                        $"The project uses a legacy encoding that requires TECKit, map file: {encodingStr}."
-                    );
-                }
-                var encoding = Encoding.GetEncoding(codePage);
+                var parser = new ZipParatextProjectSettingsParser(archive);
+                ParatextProjectSettings settings = parser.Parse();
 
-                var scrVersType = (int?)settingsDoc.Root.Element("Versification") ?? (int)ScrVersType.English;
-                Versification = new ScrVers((ScrVersType)scrVersType);
-                ZipArchiveEntry customVersEntry = archive.GetEntry("custom.vrs");
-                if (customVersEntry != null)
-                {
-                    var guid = (string)settingsDoc.Root.Element("Guid");
-                    string versName = ((ScrVersType)scrVersType).ToString() + "-" + guid;
-                    if (Scripture.Versification.Table.Implementation.Exists(versName))
-                    {
-                        Versification = new ScrVers(versName);
-                    }
-                    else
-                    {
-                        using (var reader = new StreamReader(customVersEntry.Open()))
-                        {
-                            Versification = Scripture.Versification.Table.Implementation.Load(
-                                reader,
-                                "custom.vrs",
-                                Versification,
-                                versName
-                            );
-                        }
-                    }
-                }
+                Versification = settings.Versification;
 
-                var stylesheetName = (string)settingsDoc.Root.Element("StyleSheet") ?? "usfm.sty";
-                ZipArchiveEntry stylesheetEntry = archive.GetEntry(stylesheetName);
-                if (stylesheetEntry == null && stylesheetName != "usfm_sb.sty")
-                    stylesheetEntry = archive.GetEntry("usfm.sty");
-                ZipArchiveEntry customStylesheetEntry = archive.GetEntry("custom.sty");
-
-                UsfmStylesheet stylesheet;
-                using (var stylesheetTempFile = TempFile.CreateAndGetPathButDontMakeTheFile())
-                using (var customStylesheetTempFile = TempFile.CreateAndGetPathButDontMakeTheFile())
-                {
-                    string stylesheetPath = "usfm.sty";
-                    if (stylesheetEntry != null)
-                    {
-                        stylesheetEntry.ExtractToFile(stylesheetTempFile.Path);
-                        stylesheetPath = stylesheetTempFile.Path;
-                    }
-                    string customStylesheetPath = null;
-                    if (customStylesheetEntry != null)
-                    {
-                        customStylesheetEntry.ExtractToFile(customStylesheetTempFile.Path);
-                        customStylesheetPath = customStylesheetTempFile.Path;
-                    }
-                    stylesheet = new UsfmStylesheet(stylesheetPath, customStylesheetPath);
-                }
-
-                string prefix = "";
-                string suffix = ".SFM";
-                XElement namingElem = settingsDoc.Root.Element("Naming");
-                if (namingElem != null)
-                {
-                    var prePart = (string)namingElem.Attribute("PrePart");
-                    if (!string.IsNullOrEmpty(prePart))
-                        prefix = prePart;
-                    var postPart = (string)namingElem.Attribute("PostPart");
-                    if (!string.IsNullOrEmpty(postPart))
-                        suffix = postPart;
-                }
-
-                var regex = new Regex($"^{Regex.Escape(prefix)}.*{Regex.Escape(suffix)}$");
+                var regex = new Regex(
+                    $"^{Regex.Escape(settings.FileNamePrefix)}.*{Regex.Escape(settings.FileNameSuffix)}$"
+                );
 
                 foreach (ZipArchiveEntry sfmEntry in archive.Entries.Where(e => regex.IsMatch(e.FullName)))
                 {
                     AddText(
                         new UsfmZipText(
-                            stylesheet,
-                            encoding,
+                            settings.Stylesheet,
+                            settings.Encoding,
                             fileName,
                             sfmEntry.FullName,
                             Versification,
---
+++ src/SIL.Machine/Corpora/UsfmVerseTextUpdater.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SIL.Scripture;
+
+namespace SIL.Machine.Corpora
+{
+    public class UsfmVerseTextUpdater : UsfmParserHandlerBase
+    {
+        private readonly IReadOnlyList<(IReadOnlyList<VerseRef>, string)> _rows;
+        private readonly List<UsfmToken> _tokens;
+        private readonly string _idText;
+        private readonly bool _stripAllText;
+        private int _rowIndex;
+        private int _tokenIndex;
+        private bool _replaceText;
+
+        public UsfmVerseTextUpdater(
+            IReadOnlyList<(IReadOnlyList<VerseRef>, string)> rows = null,
+            string idText = null,
+            bool stripAllText = false
+        )
+        {
+            _rows = rows ?? Array.Empty<(IReadOnlyList<VerseRef>, string)>();
+            _tokens = new List<UsfmToken>();
+            _idText = idText;
+            _stripAllText = stripAllText;
+        }
+
+        public IReadOnlyList<UsfmToken> Tokens => _tokens;
+
+        public override void StartBook(UsfmParserState state, string marker, string code)
+        {
+            CollectTokens(state);
+            if (_idText != null)
+            {
+                _tokens.Add(new UsfmToken(_idText + " "));
+                _replaceText = true;
+            }
+        }
+
+        public override void EndBook(UsfmParserState state, string marker)
+        {
+            _replaceText = false;
+        }
+
+        public override void StartPara(
+            UsfmParserState state,
+            string marker,
+            bool unknown,
+            IReadOnlyList<UsfmAttribute> attributes
+        )
+        {
+            if (!state.IsVersePara)
+                _replaceText = false;
+            CollectTokens(state);
+        }
+
+        public override void StartRow(UsfmParserState state, string marker)
+        {
+            CollectTokens(state);
+        }
+
+        public override void StartCell(UsfmParserState state, string marker, string align, int colspan)
+        {
+            CollectTokens(state);
+        }
+
+        public override void EndCell(UsfmParserState state, string marker)
+        {
+            CollectTokens(state);
+        }
+
+        public override void StartSidebar(UsfmParserState state, string marker, string category)
+        {
+            _replaceText = false;
+            CollectTokens(state);
+        }
+
+        public override void EndSidebar(UsfmParserState state, string marker, bool closed)
+        {
+            _replaceText = false;
+            if (closed)
+                CollectTokens(state);
+        }
+
+        public override void Chapter(
+            UsfmParserState state,
+            string number,
+            string marker,
+            string altNumber,
+            string pubNumber
+        )
+        {
+            _replaceText = false;
+            CollectTokens(state);
+        }
+
+        public override void Milestone(
+            UsfmParserState state,
+            string marker,
+            bool startMilestone,
+            IReadOnlyList<UsfmAttribute> attributes
+        )
+        {
+            _replaceText = false;
+            CollectTokens(state);
+        }
+
+        public override void Verse(
+            UsfmParserState state,
+            string number,
+            string marker,
+            string altNumber,
+            string pubNumber
+        )
+        {
+            _replaceText = false;
+            CollectTokens(state);
+
+            while (_rowIndex < _rows.Count)
+            {
+                var (verseRefs, text) = _rows[_rowIndex];
+                bool stop = false;
+                foreach (VerseRef verseRef in verseRefs)
+                {
+                    int compare = verseRef.CompareTo(state.VerseRef, compareAllVerses: true);
+                    if (compare == 0)
+                    {
+                        _tokens.Add(new UsfmToken(text + " "));
+                        _replaceText = true;
+                        break;
+                    }
+                    else
+                    {
+                        if (state.VerseRef.AllVerses().Any(v => v.Equals(verseRef)))
+                        {
+                            _tokens.Add(new UsfmToken(text + " "));
+                            _replaceText = true;
+                            break;
+                        }
+                        if (compare > 0)
+                        {
+                            stop = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (stop)
+                    break;
+                else
+                    _rowIndex++;
+            }
+        }
+
+        public override void StartChar(
+            UsfmParserState state,
+            string markerWithoutPlus,
+            bool unknown,
+            IReadOnlyList<UsfmAttribute> attributes
+        )
+        {
+            // strip out char-style markers in verses that are being replaced
+            if (_stripAllText || (_replaceText && state.IsVersePara))
+                SkipTokens(state);
+            else
+                CollectTokens(state);
+        }
+
+        public override void EndChar(
+            UsfmParserState state,
+            string marker,
+            IReadOnlyList<UsfmAttribute> attributes,
+            bool closed
+        )
+        {
+            // strip out char-style markers in verses that are being replaced
+            if (closed && (_stripAllText || (_replaceText && state.IsVersePara)))
+                SkipTokens(state);
+        }
+
+        public override void StartNote(UsfmParserState state, string marker, string caller, string category)
+        {
+            // strip out notes in verses that are being replaced
+            if (_stripAllText || (_replaceText && state.IsVersePara))
+                SkipTokens(state);
+            else
+                CollectTokens(state);
+        }
+
+        public override void EndNote(UsfmParserState state, string marker, bool closed)
+        {
+            // strip out notes in verses that are being replaced
+            if (closed && (_stripAllText || (_replaceText && state.IsVersePara)))
+                SkipTokens(state);
+        }
+
+        public override void Ref(UsfmParserState state, string marker, string display, string target)
+        {
+            // strip out ref in verses that are being replaced
+            if (_stripAllText || (_replaceText && state.IsVersePara))
+                SkipTokens(state);
+            else
+                CollectTokens(state);
+        }
+
+        public override void Text(UsfmParserState state, string text)
+        {
+            // strip out text in verses that are being replaced
+            if (_stripAllText || (_replaceText && (state.IsVersePara || state.ParaTag.Marker == "id")))
+                SkipTokens(state);
+            else
+                CollectTokens(state);
+        }
+
+        public override void OptBreak(UsfmParserState state)
+        {
+            // strip out optbreaks in verses that are being replaced
+            if (_stripAllText || (_replaceText && state.IsVersePara))
+                SkipTokens(state);
+            else
+                CollectTokens(state);
+        }
+
+        public override void Unmatched(UsfmParserState state, string marker)
+        {
+            if (!state.IsVersePara)
+                _replaceText = false;
+            CollectTokens(state);
+        }
+
+        public string GetUsfm(string stylesheetFileName = "usfm.sty")
+        {
+            return GetUsfm(new UsfmStylesheet(stylesheetFileName));
+        }
+
+        public string GetUsfm(UsfmStylesheet stylesheet)
+        {
+            var tokenizer = new UsfmTokenizer(stylesheet);
+            return tokenizer.Detokenize(_tokens);
+        }
+
+        private void CollectTokens(UsfmParserState state)
+        {
+            while (_tokenIndex <= state.Index + state.SpecialTokenCount)
+            {
+                _tokens.Add(state.Tokens[_tokenIndex]);
+                _tokenIndex++;
+            }
+        }
+
+        private void SkipTokens(UsfmParserState state)
+        {
+            _tokenIndex = state.Index + 1 + state.SpecialTokenCount;
+        }
+    }
+}
