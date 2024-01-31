ff49f39793df29c74d18064d370bebaaae7afc8b
Damien Daspit <damien_daspit@sil.org>
2024-01-31 17:39:26 -0500

    Add functionality to Paratext project settings parser
    - add ParatextProjectSettings.GetBookFileName()
    - add ZipParatextProjectSettingsParserBase class

---
+++ src/SIL.Machine/Corpora/ZipParatextProjectSettingsParserBase.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using SIL.IO;
+
+namespace SIL.Machine.Corpora
+{
+    public abstract class ZipParatextProjectSettingsParserBase : ParatextProjectSettingsParserBase
+    {
+        protected override UsfmStylesheet CreateStylesheet(string fileName)
+        {
+            TempFile stylesheetTempFile = null;
+            TempFile customStylesheetTempFile = null;
+            try
+            {
+                string stylesheetPath = fileName;
+                if (Exists(fileName))
+                {
+                    stylesheetTempFile = TempFile.CreateAndGetPathButDontMakeTheFile();
+                    using (Stream source = Open(fileName))
+                    using (Stream target = File.OpenWrite(stylesheetTempFile.Path))
+                    {
+                        source.CopyTo(target);
+                    }
+                    stylesheetPath = stylesheetTempFile.Path;
+                }
+
+                string customStylesheetPath = null;
+                if (Exists("custom.sty"))
+                {
+                    customStylesheetTempFile = TempFile.CreateAndGetPathButDontMakeTheFile();
+                    using (Stream source = Open("custom.sty"))
+                    using (Stream target = File.OpenWrite(customStylesheetTempFile.Path))
+                    {
+                        source.CopyTo(target);
+                    }
+                    customStylesheetPath = customStylesheetTempFile.Path;
+                }
+                return new UsfmStylesheet(stylesheetPath, customStylesheetPath);
+            }
+            finally
+            {
+                stylesheetTempFile?.Dispose();
+                customStylesheetTempFile?.Dispose();
+            }
+        }
+    }
+}
