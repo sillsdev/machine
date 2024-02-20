cc909d2c617a2f5192f6431f77267c26c5eef189
John Lambert <john_lambert@sil.org>
2024-02-20 13:14:30 -0500

    Better error handling for download endpoint In e2e test, grab current machine.py version

--- src/SIL.Machine.AspNetCore/Configuration/IMachineBuilderExtensions.cs
+++ src/SIL.Machine.AspNetCore/Configuration/IMachineBuilderExtensions.cs
@@ -336,6 +336,7 @@ public static class IMachineBuilderExtensions
         {
             options.Interceptors.Add<CancellationInterceptor>();
             options.Interceptors.Add<UnimplementedInterceptor>();
+            options.Interceptors.Add<InvalidOperationInterceptor>();
         });
         builder.AddServalPlatformService(connectionString);
         engineTypes ??=
---
+++ src/SIL.Machine.AspNetCore/Services/InvalidOperationInterceptor.cs
@@ -0,0 +1,20 @@
+﻿namespace SIL.Machine.AspNetCore.Services;
+
+public class InvalidOperationInterceptor : Interceptor
+{
+    public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
+        TRequest request,
+        ServerCallContext context,
+        UnaryServerMethod<TRequest, TResponse> continuation
+    )
+    {
+        try
+        {
+            return await continuation(request, context);
+        }
+        catch (InvalidOperationException)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "This operation is not valid at this time."));
+        }
+    }
+}
