8b2765f7b2acc5eddd7bc1133b2d86b671106ae5
John Lambert <john_lambert@sil.org>
2024-02-20 13:14:30 -0500

    Better error handling for download endpoint In e2e test, grab current machine.py version

--- src/SIL.Machine.AspNetCore/Services/ServalTranslationEngineServiceV1.cs
+++ src/SIL.Machine.AspNetCore/Services/ServalTranslationEngineServiceV1.cs
@@ -150,6 +150,10 @@ public class ServalTranslationEngineServiceV1(
         {
             throw new RpcException(new Status(StatusCode.Aborted, e.Message));
         }
+        catch (FileNotFoundException e)
+        {
+            throw new RpcException(new Status(StatusCode.DataLoss, e.Message));
+        }
     }
 
     public override async Task<GetQueueSizeResponse> GetQueueSize(
