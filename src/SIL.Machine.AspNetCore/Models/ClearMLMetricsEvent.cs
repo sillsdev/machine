6f3ebf1abbec7361019aab2c441c09aaf14ca1ee
John Lambert <john_lambert@sil.org>
2024-02-27 13:48:42 -0500

    fix #332 - only require fields used (#175)

--- src/SIL.Machine.AspNetCore/Models/ClearMLMetricsEvent.cs
+++ src/SIL.Machine.AspNetCore/Models/ClearMLMetricsEvent.cs
@@ -2,11 +2,11 @@
 
 public record ClearMLMetricsEvent
 {
-    public required string Metric { get; init; }
-    public required string Variant { get; init; }
+    public string? Metric { get; init; }
+    public string? Variant { get; init; }
     public required double Value { get; init; }
-    public required double MinValue { get; init; }
-    public required int MinValueIteration { get; init; }
-    public required double MaxValue { get; init; }
-    public required int MaxValueIteration { get; init; }
+    public double? MinValue { get; init; }
+    public int? MinValueIteration { get; init; }
+    public double? MaxValue { get; init; }
+    public int? MaxValueIteration { get; init; }
 }
