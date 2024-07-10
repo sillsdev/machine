156cbecd90000c45dfd25988c02c8c06e97eaf35
John Lambert <john_lambert@sil.org>
2024-07-10 15:45:44 -0400

    Move to tokenization tests

--- tests/SIL.Machine.Tests/Corpora/UsfmTokenizerTests.cs
+++ tests/SIL.Machine.Tests/Corpora/UsfmTokenizerTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Text;
+using NUnit.Framework;
 
 namespace SIL.Machine.Corpora;
 
@@ -47,8 +48,39 @@ public class UsfmTokenizerTests
         Assert.That(result, Is.EqualTo(usfm));
     }
 
+    [Test]
+    public void Tokenize_Ending_ParagraphMarker()
+    {
+        //The ending paragraph marker should not crash the parser.
+        TextRow[] rows = GetRows(
+            @"\id MAT - Test
+\c 1
+\v 1 Descriptive title\x - \xo 18:16 \xt  hello world\x*\p
+"
+        );
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(rows, Has.Length.EqualTo(1));
+        });
+    }
+
     private static string ReadUsfm()
     {
         return File.ReadAllText(Path.Combine(CorporaTestHelpers.UsfmTestProjectPath, "41MATTes.SFM"));
+    }
+
+    private static TextRow[] GetRows(string usfm, bool includeMarkers = false, bool includeAllText = false)
+    {
+        UsfmMemoryText text =
+            new(
+                new UsfmStylesheet("usfm.sty"),
+                Encoding.UTF8,
+                "MAT",
+                usfm.Trim().ReplaceLineEndings("\r\n") + "\r\n",
+                includeMarkers: includeMarkers,
+                includeAllText: includeAllText
+            );
+        return text.GetRows().ToArray();
     }
 }
