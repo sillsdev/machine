b140176a1907227d5c4e5f4a83a1dd2aa637b452
Enkidu93 <lowryec17@gcc.edu>
2024-08-01 10:48:57 -0400

    Draft: Fill in zip classes

---
+++ src/SIL.Machine/Corpora/ZipParatextProjecTextUpdater.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.IO.Compression;
+
+namespace SIL.Machine.Corpora
+{
+    public class ZipParatextProjectTextUpdater : ZipParatextProjectTextUpdaterBase
+    {
+        private readonly ZipArchive _archive;
+
+        public ZipParatextProjectTextUpdater(ZipArchive archive)
+            : base(new ZipParatextProjectSettingsParser(archive))
+        {
+            _archive = archive;
+        }
+
+        protected override bool Exists(string fileName)
+        {
+            return _archive.GetEntry(fileName) != null;
+        }
+
+        protected override Stream Open(string fileName)
+        {
+            ZipArchiveEntry entry = _archive.GetEntry(fileName);
+            if (entry == null)
+                return null;
+            return entry.Open();
+        }
+    }
+}
---
+++ src/SIL.Machine/Corpora/ZipParatextProjectTextUpdaterBase.cs
@@ -0,0 +1,8 @@
+namespace SIL.Machine.Corpora
+{
+    public abstract class ZipParatextProjectTextUpdaterBase : ParatextProjectTextUpdaterBase
+    {
+        protected ZipParatextProjectTextUpdaterBase(ParatextProjectSettingsParserBase settingsParser)
+            : base(settingsParser) { }
+    }
+}
