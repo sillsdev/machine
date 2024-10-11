4c31e2f1a8559b32502907d9ed0749824d17a8ab
John Lambert <john_lambert@sil.org>
2024-10-11 15:10:54 -0400

    Add test

---
+++ tests/SIL.Machine.Tests/Utils/EnumberableExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace SIL.Machine.Utils;
+
+[TestFixture]
+public class EnumerableExtensionTests
+{
+    [Test]
+    public void ZipMany_None()
+    {
+        var seqs = new List<IEnumerable<int>>();
+        IEnumerable<int> result = seqs.ZipMany(x => x.Sum());
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void ZipMany_Two()
+    {
+        var seqs = new List<IEnumerable<int>> { new[] { 1, 2, 3 }, new[] { 4, 5, 6 } };
+        IEnumerable<int> result = seqs.ZipMany(x => x.Sum());
+        Assert.That(result, Is.EqualTo(new[] { 5, 7, 9 }));
+    }
+}
