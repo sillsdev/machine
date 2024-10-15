86899d4e70ac99d9866138b20f2780c20a9a2f85
John Lambert <john_lambert@sil.org>
2024-10-15 14:30:57 -0400

    a start

---
+++ src/SIL.Machine/Translation/IWordAlignerEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SIL.Machine.Translation
+{
+    public interface IWordAlignerEngine : IDisposable
+    {
+        Task<TranslationResult> GetBestPhraseAlignmentAsync(
+            string sourceSegment,
+            string targetSegment,
+            CancellationToken cancellationToken = default
+        );
+
+        Task<TranslationResult> GetBestPhraseAlignmentAsync(
+            IReadOnlyList<string> sourceSegment,
+            IReadOnlyList<string> targetSegment,
+            CancellationToken cancellationToken = default
+        );
+
+        TranslationResult GetBestPhraseAlignment(string sourceSegment, string targetSegment);
+
+        TranslationResult GetBestPhraseAlignment(
+            IReadOnlyList<string> sourceSegment,
+            IReadOnlyList<string> targetSegment
+        );
+    }
+}
