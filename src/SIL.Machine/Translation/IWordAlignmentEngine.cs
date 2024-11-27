f49827d053de8d7ee32423763a4b73cb9fb9a0e5
John Lambert <john_lambert@sil.org>
2024-11-27 12:34:15 -0500

    a start

---
+++ src/SIL.Machine/Translation/IWordAlignmentEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SIL.Machine.Translation
+{
+    public interface IWordAlignmentEngine : IWordAligner, IDisposable
+    {
+        Task<WordAlignmentResult> GetBestAlignmentAsync(
+            string sourceSegment,
+            string targetSegment,
+            CancellationToken cancellationToken = default
+        );
+
+        Task<WordAlignmentResult> GetBestAlignmentAsync(
+            IReadOnlyList<string> sourceSegment,
+            IReadOnlyList<string> targetSegment,
+            CancellationToken cancellationToken = default
+        );
+
+        WordAlignmentResult GetBestAlignment(string sourceSegment, string targetSegment);
+
+        WordAlignmentResult GetBestAlignment(IReadOnlyList<string> sourceSegment, IReadOnlyList<string> targetSegment);
+    }
+}
---
+++ src/SIL.Machine/Translation/WordAlignmentResult.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SIL.Machine.Translation
+{
+    public class WordAlignmentResult
+    {
+        public WordAlignmentResult(
+            IEnumerable<string> sourceTokens,
+            IEnumerable<string> targetTokens,
+            IEnumerable<double> confidences,
+            WordAlignmentMatrix alignment
+        )
+        {
+            SourceTokens = sourceTokens.ToArray();
+            TargetTokens = targetTokens.ToArray();
+            Confidences = confidences.ToArray();
+            if (Confidences.Count != TargetTokens.Count)
+            {
+                throw new ArgumentException(
+                    "The confidences must be the same length as the target segment.",
+                    nameof(confidences)
+                );
+            }
+            Alignment = alignment;
+            if (Alignment.RowCount != SourceTokens.Count)
+            {
+                throw new ArgumentException(
+                    "The alignment source length must be the same length as the source segment.",
+                    nameof(alignment)
+                );
+            }
+            if (Alignment.ColumnCount != TargetTokens.Count)
+            {
+                throw new ArgumentException(
+                    "The alignment target length must be the same length as the target segment.",
+                    nameof(alignment)
+                );
+            }
+        }
+
+        public IReadOnlyList<string> SourceTokens { get; }
+        public IReadOnlyList<string> TargetTokens { get; }
+        public IReadOnlyList<double> Confidences { get; }
+        public WordAlignmentMatrix Alignment { get; }
+    }
+}
