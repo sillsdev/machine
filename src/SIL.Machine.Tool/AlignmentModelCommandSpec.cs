f905c2da800d3a9b461009cb9e722277c6f75de4
John Lambert <john_lambert@sil.org>
2024-11-27 12:34:15 -0500

    a start Add tokenizer to trainer

--- src/SIL.Machine.Tool/AlignmentModelCommandSpec.cs
+++ src/SIL.Machine.Tool/AlignmentModelCommandSpec.cs
@@ -37,7 +37,7 @@ public class AlignmentModelCommandSpec : ICommandSpec
         _modelArgument = command.Argument("MODEL_PATH", "The word alignment model.").IsRequired();
         _modelTypeOption = command.Option(
             "-mt|--model-type <MODEL_TYPE>",
-            $"The word alignment model type.\nTypes: \"{ToolHelpers.Hmm}\" (default), \"{ToolHelpers.Ibm1}\", \"{ToolHelpers.Ibm2}\", \"{ToolHelpers.Ibm3}\", \"{ToolHelpers.Ibm4}\", \"{ToolHelpers.FastAlign}\".",
+            $"The word alignment model type.\nTypes: \"{ThotWordAlignmentHelpers.Hmm}\" (default), \"{ThotWordAlignmentHelpers.Ibm1}\", \"{ThotWordAlignmentHelpers.Ibm2}\", \"{ThotWordAlignmentHelpers.Ibm3}\", \"{ThotWordAlignmentHelpers.Ibm4}\", \"{ThotWordAlignmentHelpers.FastAlign}\".",
             CommandOptionType.SingleValue
         );
         _pluginOption = command.Option(
@@ -90,7 +90,7 @@ public class AlignmentModelCommandSpec : ICommandSpec
         ThotWordAlignmentModelType modelType = ThotWordAlignmentModelType.Hmm;
         if (_modelTypeOption.HasValue())
         {
-            modelType = ToolHelpers.GetThotWordAlignmentModelType(_modelTypeOption.Value());
+            modelType = ThotWordAlignmentHelpers.GetThotWordAlignmentModelType(_modelTypeOption.Value());
         }
         else
         {
@@ -103,7 +103,7 @@ public class AlignmentModelCommandSpec : ICommandSpec
                     yaml.Load(reader);
                     var root = (YamlMappingNode)yaml.Documents.First().RootNode;
                     var modelTypeStr = (string)root[new YamlScalarNode("model")];
-                    modelType = ToolHelpers.GetThotWordAlignmentModelType(modelTypeStr);
+                    modelType = ThotWordAlignmentHelpers.GetThotWordAlignmentModelType(modelTypeStr);
                 }
             }
         }
@@ -144,7 +144,9 @@ public class AlignmentModelCommandSpec : ICommandSpec
             return _modelFactory.CreateTrainer(_modelArgument.Value, corpus, maxSize, parameters, direct);
         }
 
-        ThotWordAlignmentModelType modelType = ToolHelpers.GetThotWordAlignmentModelType(_modelTypeOption.Value());
+        ThotWordAlignmentModelType modelType = ThotWordAlignmentHelpers.GetThotWordAlignmentModelType(
+            _modelTypeOption.Value()
+        );
 
         string modelPath = _modelArgument.Value;
         if (ToolHelpers.IsDirectoryPath(modelPath))
@@ -227,12 +229,12 @@ public class AlignmentModelCommandSpec : ICommandSpec
     {
         var validTypes = new HashSet<string>
         {
-            ToolHelpers.Hmm,
-            ToolHelpers.Ibm1,
-            ToolHelpers.Ibm2,
-            ToolHelpers.FastAlign,
-            ToolHelpers.Ibm3,
-            ToolHelpers.Ibm4
+            ThotWordAlignmentHelpers.Hmm,
+            ThotWordAlignmentHelpers.Ibm1,
+            ThotWordAlignmentHelpers.Ibm2,
+            ThotWordAlignmentHelpers.FastAlign,
+            ThotWordAlignmentHelpers.Ibm3,
+            ThotWordAlignmentHelpers.Ibm4
         };
         validTypes.UnionWith(pluginTypes);
         return string.IsNullOrEmpty(value) || validTypes.Contains(value);
--- src/SIL.Machine/Translation/IWordAlignmentModel.cs
+++ src/SIL.Machine/Translation/IWordAlignmentModel.cs
@@ -1,32 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using SIL.Machine.Corpora;
-using SIL.ObjectModel;
+﻿using SIL.Machine.Corpora;
 
 namespace SIL.Machine.Translation
 {
-    public interface IWordAlignmentModel : IWordAligner, IDisposable
+    public interface IWordAlignmentModel : IWordAlignmentEngine
     {
-        IWordVocabulary SourceWords { get; }
-        IWordVocabulary TargetWords { get; }
-        IReadOnlySet<int> SpecialSymbolIndices { get; }
-
         ITrainer CreateTrainer(IParallelTextCorpus corpus);
-
-        IEnumerable<(string TargetWord, double Score)> GetTranslations(string sourceWord, double threshold = 0);
-        IEnumerable<(int TargetWordIndex, double Score)> GetTranslations(int sourceWordIndex, double threshold = 0);
-
-        double GetTranslationScore(string sourceWord, string targetWord);
-        double GetTranslationScore(int sourceWordIndex, int targetWordIndex);
-
-        IReadOnlyCollection<AlignedWordPair> GetBestAlignedWordPairs(
-            IReadOnlyList<string> sourceSegment,
-            IReadOnlyList<string> targetSegment
-        );
-        void ComputeAlignedWordPairScores(
-            IReadOnlyList<string> sourceSegment,
-            IReadOnlyList<string> targetSegment,
-            IReadOnlyCollection<AlignedWordPair> wordPairs
-        );
     }
 }
