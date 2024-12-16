e19384602e56b9e19a5542b485b13b13f73f6cea
Mudiaga Obriki <mudiaga_obriki@sil.org>
2024-12-16 18:36:47 +0100

    Wrote necessary plus some additional tests to investigate possible alignment and versification issues with deuterocanonical books

---
+++ src/SIL.Machine/Scripture/ScVersExtensions.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace SIL.Scripture.Extensions
+{
+    public static class ScrVersExtensions
+    {
+        /// <summary>
+        /// Gets a list of references (verse references) for the specified book.
+        /// </summary>
+        public static IEnumerable<VerseRef> GetReferencesForBook(this ScrVers scrVers, int bookNum)
+        {
+            List<VerseRef> references = new List<VerseRef>();
+            int lastChapter = scrVers.GetLastChapter(bookNum);
+
+            for (int chapterNum = 1; chapterNum <= lastChapter; chapterNum++)
+            {
+                int lastVerse = scrVers.GetLastVerse(bookNum, chapterNum);
+
+                for (int verseNum = 1; verseNum <= lastVerse; verseNum++)
+                {
+                    int bbbcccvvv = VerseRef.GetBBBCCCVVV(bookNum, chapterNum, verseNum);
+                    if (!scrVers.IsExcluded(bbbcccvvv))
+                    {
+                        references.Add(new VerseRef(bookNum, chapterNum, verseNum, scrVers));
+                    }
+                }
+            }
+
+            return references;
+        }
+    }
+}
