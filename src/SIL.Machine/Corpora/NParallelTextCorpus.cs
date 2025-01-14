fa1d6c7fb601f544bc0b7d226a23bc27ed3271ec
Enkidu93 <lowryec17@gcc.edu>
2025-01-14 09:03:50 -0500

    Small bug in NParallelTextCorpus

--- src/SIL.Machine/Corpora/NParallelTextCorpus.cs
+++ src/SIL.Machine/Corpora/NParallelTextCorpus.cs
@@ -186,6 +186,8 @@ namespace SIL.Machine.Corpora
                     }
                     foreach (int i in minRefIndexes)
                     {
+                        if (completed[i])
+                            continue;
                         sameRefRows[i].Add(enumerators[i].Current);
                         bool isCompleted = !enumerators[i].MoveNext();
                         completed[i] = isCompleted;
@@ -241,6 +243,8 @@ namespace SIL.Machine.Corpora
 
                     for (int i = 0; i < rangeInfo.Rows.Count; i++)
                     {
+                        if (completed[i])
+                            continue;
                         sameRefRows[i].Add(currentRows[i]);
                         bool isCompleted = !enumerators[i].MoveNext();
                         completed[i] = isCompleted;
