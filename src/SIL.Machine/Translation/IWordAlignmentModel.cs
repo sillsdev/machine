92e62ca719baa4f7f0506a5e383fe25181a20ccf
Enkidu93 <lowryec17@gcc.edu>
2025-02-10 09:57:56 -0500

    Remove IWordAlignmentEngine

--- src/SIL.Machine/Translation/IWordAlignmentModel.cs
+++ src/SIL.Machine/Translation/IWordAlignmentModel.cs
@@ -1,13 +1,32 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
 using SIL.Machine.Corpora;
+using SIL.ObjectModel;
 
 namespace SIL.Machine.Translation
 {
-    public interface IWordAlignmentModel : IWordAlignmentEngine
+    public interface IWordAlignmentModel : IWordAligner, IDisposable
     {
+        IWordVocabulary SourceWords { get; }
+        IWordVocabulary TargetWords { get; }
+        IReadOnlySet<int> SpecialSymbolIndices { get; }
+
         ITrainer CreateTrainer(IParallelTextCorpus corpus);
-        Task SaveAsync(CancellationToken cancellationToken = default);
-        void Save();
+
+        IEnumerable<(string TargetWord, double Score)> GetTranslations(string sourceWord, double threshold = 0);
+        IEnumerable<(int TargetWordIndex, double Score)> GetTranslations(int sourceWordIndex, double threshold = 0);
+
+        double GetTranslationScore(string sourceWord, string targetWord);
+        double GetTranslationScore(int sourceWordIndex, int targetWordIndex);
+
+        IReadOnlyCollection<AlignedWordPair> GetBestAlignedWordPairs(
+            IReadOnlyList<string> sourceSegment,
+            IReadOnlyList<string> targetSegment
+        );
+        void ComputeAlignedWordPairScores(
+            IReadOnlyList<string> sourceSegment,
+            IReadOnlyList<string> targetSegment,
+            IReadOnlyCollection<AlignedWordPair> wordPairs
+        );
     }
 }
