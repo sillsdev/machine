24c94741721678e7eff50db099c8c655b5ac9df2
Enkidu93 <lowryec17@gcc.edu>
2025-02-21 14:20:07 -0500

    Clean up logic; add assert requested

--- tests/SIL.Machine.Tests/Corpora/AlignedWordPairTests.cs
+++ tests/SIL.Machine.Tests/Corpora/AlignedWordPairTests.cs
@@ -14,6 +14,7 @@ public class AlignedWordPairTests
             Is.True
         );
         Assert.That(alignedWordPairs, Has.Count.EqualTo(3));
+        Assert.That(alignedWordPairs.First().TranslationScore, Is.EqualTo(0.111111));
         Assert.That(alignedWordPairs.First().AlignmentScore, Is.EqualTo(0.222222));
         alignmentString = "1-0:0.111111";
         Assert.That(AlignedWordPair.TryParse(alignmentString, out alignedWordPairs), Is.True);
