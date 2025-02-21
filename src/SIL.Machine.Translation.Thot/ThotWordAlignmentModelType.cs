37b9e09893c831115ac0a7f02d5027fe833479f3
Enkidu93 <lowryec17@gcc.edu>
2025-02-21 12:55:58 -0500

    Add ThotWAModelType helper

--- src/SIL.Machine.Translation.Thot/ThotWordAlignmentModelType.cs
+++ src/SIL.Machine.Translation.Thot/ThotWordAlignmentModelType.cs
@@ -9,4 +9,28 @@
         Ibm3,
         Ibm4
     }
+
+    public class ThotWordAlignmentModelTypeHelpers
+    {
+        public static ThotWordAlignmentModelType GetThotWordAlignmentModelType(string modelType)
+        {
+            switch (modelType)
+            {
+                case "fastAlign":
+                case "fast_align":
+                    return ThotWordAlignmentModelType.FastAlign;
+                case "ibm1":
+                    return ThotWordAlignmentModelType.Ibm1;
+                case "ibm2":
+                    return ThotWordAlignmentModelType.Ibm2;
+                default:
+                case "hmm":
+                    return ThotWordAlignmentModelType.Hmm;
+                case "ibm3":
+                    return ThotWordAlignmentModelType.Ibm3;
+                case "ibm4":
+                    return ThotWordAlignmentModelType.Ibm4;
+            }
+        }
+    }
 }
