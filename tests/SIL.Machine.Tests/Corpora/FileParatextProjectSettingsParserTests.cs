53294c22202c6b2e906a097bb061e414be3523ed
Damien Daspit <damien_daspit@sil.org>
2025-09-17 11:25:55 -0400

    Use correct filename for custom stylesheet

---
+++ tests/SIL.Machine.Tests/Corpora/FileParatextProjectSettingsParserTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace SIL.Machine.Corpora;
+
+public class FileParatextProjectSettingsParserTests
+{
+    [Test]
+    public void Parse_CustomStylesheet()
+    {
+        FileParatextProjectSettingsParser parser = new(CorporaTestHelpers.UsfmTestProjectPath);
+        ParatextProjectSettings settings = parser.Parse();
+        UsfmTag testTag = settings.Stylesheet.GetTag("test");
+        Assert.That(testTag.StyleType, Is.EqualTo(UsfmStyleType.Character));
+        Assert.That(testTag.TextType, Is.EqualTo(UsfmTextType.Other));
+    }
+}
