5e5db10778f6c71a8a1dad5be5cecd9437109225
Peter Chapman <peter@conglomo.co.nz>
2026-03-24 11:46:35 +1300

    Refactored quality estimation

---
+++ src/SIL.Machine/QualityEstimation/Thresholds.cs
@@ -0,0 +1,20 @@
+﻿namespace SIL.Machine.QualityEstimation
+{
+    public class Thresholds
+    {
+        public Thresholds(double greenThreshold, double yellowThreshold)
+        {
+            GreenThreshold = greenThreshold;
+            YellowThreshold = yellowThreshold;
+        }
+
+        public double GreenThreshold { get; }
+
+        public double YellowThreshold { get; }
+
+        public UsabilityLabel ReturnLabel(double probability) =>
+            probability >= GreenThreshold ? UsabilityLabel.Green
+            : probability >= YellowThreshold ? UsabilityLabel.Yellow
+            : UsabilityLabel.Red;
+    }
+}
