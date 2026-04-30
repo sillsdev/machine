66256745c000d59cffd71db76cae56908288ca57
Eli C. Lowry <83078660+Enkidu93@users.noreply.github.com>
2026-04-30 11:46:29 -0400

    Expose Visibility from Settings.xml (#412)

--- tests/SIL.Machine.Tests/Corpora/ZipParatextProjectSettingsParserTests.cs
+++ tests/SIL.Machine.Tests/Corpora/ZipParatextProjectSettingsParserTests.cs
@@ -1,11 +1,37 @@
 ﻿using System.IO.Compression;
 using NUnit.Framework;
 using SIL.ObjectModel;
+using SIL.Scripture;
 
 namespace SIL.Machine.Corpora;
 
 public class ZipParatextProjectSettingsParserTests
 {
+    [Test]
+    public void Parse()
+    {
+        using var env = new TestEnvironment();
+        ParatextProjectSettings settings = env.Parser.Parse();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(settings.Guid, Is.EqualTo("a7e0b3ce0200736062f9f810a444dbfbe64aca35"));
+            Assert.That(settings.Name, Is.EqualTo("Tes"));
+            Assert.That(settings.FullName, Is.EqualTo("Test"));
+            Assert.That(settings.Encoding, Is.EqualTo(System.Text.Encoding.UTF8));
+            Assert.That(settings.Versification.BaseVersification, Is.EqualTo(ScrVers.English));
+            Assert.That(settings.FileNamePrefix, Is.EqualTo(string.Empty));
+            Assert.That(settings.FileNameForm, Is.EqualTo("41MAT"));
+            Assert.That(settings.FileNameSuffix, Is.EqualTo("Tes.SFM"));
+            Assert.That(settings.BiblicalTermsListType, Is.EqualTo("Project"));
+            Assert.That(settings.BiblicalTermsProjectName, Is.EqualTo("Tes"));
+            Assert.That(settings.BiblicalTermsFileName, Is.EqualTo("ProjectBiblicalTerms.xml"));
+            Assert.That(settings.LanguageCode, Is.EqualTo("en"));
+            Assert.That(settings.TranslationType, Is.EqualTo("Standard"));
+            Assert.That(settings.Visibility, Is.EqualTo("Public"));
+        }
+    }
+
     [Test]
     public void Parse_CustomStylesheet()
     {
@@ -21,16 +47,15 @@ public class ZipParatextProjectSettingsParserTests
     {
         using var env = new TestEnvironment();
         ParatextProjectSettings settings = env.Parser.Parse();
+
         Assert.That(settings.HasParent);
         Assert.That(settings.IsDaughterProjectOf(settings));
-        Assert.That(settings.TranslationType, Is.EqualTo("Standard"));
         Assert.That(settings.Parent, Is.Null);
 
         env.Parser = new ZipParatextProjectSettingsParser(env.Archive, settings);
         settings = env.Parser.Parse();
         Assert.That(settings.HasParent);
         Assert.That(settings.IsDaughterProjectOf(settings));
-        Assert.That(settings.TranslationType, Is.EqualTo("Standard"));
         Assert.That(settings.Parent, Is.Not.Null);
     }
 
