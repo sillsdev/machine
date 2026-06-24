f9ba7bb7cbe9d848a5b3eecc3d43e3d041f77e33
Peter Chapman <peter@conglomo.co.nz>
2026-06-25 10:21:09 +1200

    Fix incorrect chapter numbers returned from UsfmStructureExtractor (#430)

--- tests/SIL.Machine.Tests/PunctuationAnalysis/QuotationDenormalizationTests.cs
+++ tests/SIL.Machine.Tests/PunctuationAnalysis/QuotationDenormalizationTests.cs
@@ -12,7 +12,13 @@ public class QuotationDenormalizationTests
         string normalizedUsfm =
             @"
     \id GEN
-    \c 1
+    \c 2
+    \v 1 Thus the heavens and the earth were completed in all their vast array.
+    \v 2 And by the seventh day God had finished the work He had been doing;
+    so on that day He rested from all His work.
+    \v 3 Then God blessed the seventh day and sanctified it,
+    because on that day He rested from all the work of creation that He had accomplished.
+    \c 3
     \v 1 Now the serpent was more subtle than any animal
     of the field which Yahweh God had made.
     He said to the woman, ""Has God really said,
@@ -25,7 +31,11 @@ public class QuotationDenormalizationTests
 
         string expectedDenormalizedUsfm =
             @"\id GEN
-\c 1
+\c 2
+\v 1 Thus the heavens and the earth were completed in all their vast array.
+\v 2 And by the seventh day God had finished the work He had been doing; so on that day He rested from all His work.
+\v 3 Then God blessed the seventh day and sanctified it, because on that day He rested from all the work of creation that He had accomplished.
+\c 3
 \v 1 Now the serpent was more subtle than any animal of the field which Yahweh God had made. He said to the woman, “Has God really said, ‘You shall not eat of any tree of the garden’?”
 \v 2 The woman said to the serpent, “We may eat fruit from the trees of the garden,
 \v 3 but not the fruit of the tree which is in the middle of the garden. God has said, ‘You shall not eat of it. You shall not touch it, lest you die.’”
@@ -44,7 +54,7 @@ public class QuotationDenormalizationTests
         List<(int ChapterNumber, QuotationMarkUpdateStrategy Strategy)> bestChapterStrategies =
             quotationMarkDenormalizationFirstPass.FindBestChapterStrategies();
 
-        Assert.That(bestChapterStrategies.Select(tuple => tuple.ChapterNumber).SequenceEqual([1]));
+        Assert.That(bestChapterStrategies.Select(tuple => tuple.ChapterNumber).SequenceEqual([2, 3]));
 
         var quotationMarkDenormalizer = new QuotationMarkDenormalizationUsfmUpdateBlockHandler(
             standardEnglishQuoteConvention,
