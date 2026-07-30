98372d2a39e194c5ad47faf9f94c66dc0e817a06
Peter Chapman <peter@conglomo.co.nz>
2026-07-30 16:31:43 +1200

    Port transductive alignment model

---
+++ src/SIL.Machine/Translation/ITransductiveWordAlignmentModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SIL.Machine.Translation
+{
+    public interface ITransductiveWordAlignmentModel
+    {
+        int TrainingAlignmentCount { get; }
+        WordAlignmentMatrix GetTrainingAlignment(int n);
+    }
+}
