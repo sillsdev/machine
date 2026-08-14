398734095e1f4c9a3541c48eb0a32c89447f1e6d
Enkidu93 <lowryec17@gcc.edu>
2026-08-14 16:41:42 -0400

    Remove deuterocanonicals tests (no problems found)

--- src/SIL.Machine/Corpora/ScrVersExtensions.cs
+++ src/SIL.Machine/Corpora/ScrVersExtensions.cs
@@ -5,31 +5,6 @@ namespace SIL.Machine.Corpora
 {
     public static class ScrVersExtensions
     {
-        /// <summary>
-        /// Gets a list of references (verse references) for the specified book.
-        /// </summary>
-        public static IEnumerable<VerseRef> GetReferencesForBook(this ScrVers scrVers, int bookNum)
-        {
-            List<VerseRef> references = new List<VerseRef>();
-            int lastChapter = scrVers.GetLastChapter(bookNum);
-
-            for (int chapterNum = 1; chapterNum <= lastChapter; chapterNum++)
-            {
-                int lastVerse = scrVers.GetLastVerse(bookNum, chapterNum);
-
-                for (int verseNum = 1; verseNum <= lastVerse; verseNum++)
-                {
-                    int bbbcccvvv = VerseRef.GetBBBCCCVVV(bookNum, chapterNum, verseNum);
-                    if (!scrVers.IsExcluded(bbbcccvvv))
-                    {
-                        references.Add(new VerseRef(bookNum, chapterNum, verseNum, scrVers));
-                    }
-                }
-            }
-
-            return references;
-        }
-
         public static IEnumerable<VerseRef> AllIncludedVerses(
             this ScrVers scrVers,
             Dictionary<int, HashSet<int>> onlyChapters = null
