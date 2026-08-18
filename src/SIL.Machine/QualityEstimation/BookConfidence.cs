3d6eee2cc3ea7a80bf029657ad784b7aa6a3f279
mshannon-sil <matthew_shannon@sil.org>
2026-08-17 23:34:08 -0400

    add low book confidence checker

---
+++ src/SIL.Machine/QualityEstimation/BookConfidence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SIL.Machine.QualityEstimation
+{
+    public static class BookConfidence
+    {
+        public const double LowBookConfidenceThreshold = 0.42;
+
+        public static bool IsBookConfidenceUnusuallyLow(double confidence, string bookId = null, string model = null)
+        {
+            if (!(confidence >= 0 && confidence <= 1))
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(confidence),
+                    confidence,
+                    "The book confidence is invalid. It is calculated as the geometric mean of the segment "
+                        + "confidences, and it must be between 0 and 1, inclusive."
+                );
+            }
+
+            return confidence < LowBookConfidenceThreshold;
+        }
+    }
+}
