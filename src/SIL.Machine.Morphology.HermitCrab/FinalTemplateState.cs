7c9aadd8ae9c1166beb8a2ba4d83b9b3413e92a5
John Maxwell <maxwellparc@gmail.com>
2026-09-14 08:48:28 -0700

    Remove unused values from FinalTemplateState

--- src/SIL.Machine.Morphology.HermitCrab/FinalTemplateState.cs
+++ src/SIL.Machine.Morphology.HermitCrab/FinalTemplateState.cs
@@ -1,10 +1,11 @@
 ﻿namespace SIL.Machine.Morphology.HermitCrab
 {
+    // Represent the state of a final template in analysis.
+    // This could be extended to represent the state of a final template in synthesis
+    // (replacing or implementing IsLastAppliedRuleFinal using None, FinalTemplate, NonFinalTemplate).
     enum FinalTemplateState : byte
     {
         None,
         NonTemplate,
-        FinalTemplate,
-        NonFinalTemplate,
     }
 }
